00c9aeef6445de67788edf9d164e6e269f0fb62e
eva-barczykowska <eva.barczykowska@gmail.com>
2021-12-15 15:32:21 +0100

    added solutions that I liked

--- RB100/array_average.rb
+++ RB100/array_average.rb
@@ -164,9 +164,9 @@ end
 # puts average([1, 5, 87, 45, 8, 8]) == 25
 # puts average([9, 47, 23, 95, 16, 52]) == 40
 
-puts average([1, 6])
-puts average([1, 5, 87, 45, 8, 8])
-puts average([9, 47, 23, 95, 16, 52])
+# puts average([1, 6])
+# puts average([1, 5, 87, 45, 8, 8])
+# puts average([9, 47, 23, 95, 16, 52])
 
 # a solution that I liked:
 # def average(arr)
