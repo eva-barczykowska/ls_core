4b432d5c2d600a84538fa5e71d9f81fba6ba7a48
eva-barczykowska <eva.barczykowska@gmail.com>
2021-12-15 14:21:00 +0100

    rewrote array_average method to return a float

--- RB100/array_average.rb
+++ RB100/array_average.rb
@@ -4,6 +4,7 @@
 # and returns the average of all numbers in the array.
 # The array will never be empty and the numbers will always be positive integers.
 #  Your result should also be an integer.
+# further exploration: change the method so that the return value is a float
 #
 # (Understand the) Problem
 # ----
@@ -152,10 +153,23 @@
 # reverse them
 # Code
 #
+# def average(array)
+#   array.reduce(0) { |sum, n| sum + n } / array.size
+# end
 def average(array)
-  array.reduce(0) { |sum, n| sum + n } / array.size
+  array.reduce(0) { |sum, n| sum + n } / array.size.to_f
 end
 
-puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
-puts average([1, 5, 87, 45, 8, 8]) == 25
-puts average([9, 47, 23, 95, 16, 52]) == 40
+# puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
+# puts average([1, 5, 87, 45, 8, 8]) == 25
+# puts average([9, 47, 23, 95, 16, 52]) == 40
+
+puts average([1, 6])
+puts average([1, 5, 87, 45, 8, 8])
+puts average([9, 47, 23, 95, 16, 52])
+
+# a solution that I liked:
+# def average(arr)
+#   arr.map!(&:to_f)
+#   arr.sum / arr.size
+# end
