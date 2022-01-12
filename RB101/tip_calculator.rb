307ff7d0d2449f655af6365ec8b67ea89822b39e
eva-barczykowska <eva.barczykowska@gmail.com>
2022-01-12 14:44:20 +0100

    completed further exploration part of the exercise

--- RB101/tip_calculator.rb
+++ RB101/tip_calculator.rb
@@ -16,15 +16,21 @@ bill_amount = gets.chomp.to_i
 puts "What is the tip percentage?"
 tip_percentage = gets.chomp.to_f / 100
 
-puts "Here what they input"
 tip_we_should_leave = bill_amount * tip_percentage
 total_to_pay = bill_amount + tip_we_should_leave
 
-puts "The tip is $#{tip_we_should_leave}."
-puts "The total is $#{total_to_pay}."
+puts "The tip is $#{sprintf("%1.2f", tip_percentage)}"
+puts "The total is $#{sprintf("%1.2f", total_to_pay)}"
 
+# from the docs: sprintf("%20.8f", 1234.56789) #=> "       1234.56789000"
 # What is the bill? 200
 # What is the tip percentage? 15
 #
 # The tip is $30.0
 # The total is $230.0
+
+# further exploration
+# Our solution prints the results as $30.0 and $230.0 instead of the more usual
+# $30.00 and $230.00. Modify your solution so it always prints the results with
+# 2 decimal places.
+# Hint: You will likely need Kernel#format for this.
