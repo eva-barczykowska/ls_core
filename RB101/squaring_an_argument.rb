221a6d89344baa99d19f4fbe32f2f13cee3a42e6
eva-barczykowska <eva.barczykowska@gmail.com>
2022-01-19 11:31:04 +0100

    Further exploration took me 3 hrs. Worried about my future.

--- RB101/squaring_an_argument.rb
+++ RB101/squaring_an_argument.rb
@@ -17,10 +17,37 @@ def square(n)
   multiply_two_numbers(n, n)
 end
 
-square(5) == 25
-square(-8) == 64
+# p square(5) == 25
+# p square(-8) == 64
 
 # Further Exploration
 # What if we wanted to generalize this method to a "power to the n" type method:
 # cubed, to the 4th power, to the 5th, etc. How would we go about doing so
 # while still using the multiply method?
+# if the power is just 2, I return the result of the multiply_two_numbers method
+# if the power is more than 2, I have to multiply that result
+#   (of multiply_two_numbers) further
+# further by the digit
+# I have to keep multiplying the result of the first execution of multiply_two_numbers BY DIGIT until I reach the power number
+# like (2 * 2) * 2 * 2 * 2
+
+def power_to_the_n(digit, n)
+  if n <= 2
+    result = multiply_two_numbers(digit, digit)
+  elsif n > 2
+    counter = 2
+    result = multiply_two_numbers(digit, digit)
+    until counter == n
+      result *= digit
+      counter += 1
+    end
+  end
+  result
+end
+#
+# p power_to_the_n(7, 2) == 49
+# p power_to_the_n(7, 3) == 343
+p power_to_the_n(5, 3) == 125
+p power_to_the_n(5, 4) == 625
+p power_to_the_n(5, 5) == 3125
+p power_to_the_n(4, 7) == 16384
