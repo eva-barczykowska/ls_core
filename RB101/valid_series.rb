7071f3e894d6534bf39e2f4b05d2c0644186397b
eva-barczykowska <eva.barczykowska@gmail.com>
2022-01-26 08:31:02 +0100

    further exploration

--- RB101/valid_series.rb
+++ RB101/valid_series.rb
@@ -33,3 +33,21 @@ p valid_series?([1, 12, 2, 5, 16, 6])             # should return false
 p valid_series?([28, 3, 4, 7, 9, 14])             # should return false
 p valid_series?([20, 6, 9, 4, 2, 1, 2, 3])        # should return true
 p valid_series?([10, 6, 19, 2, 6, 4])             # should return false
+
+#further Exploration
+# If you take a close look at odd_count == 3 ? true : false, you see that the
+# equality comparison odd_count == 3 itself returns a Boolean. Is the ternary
+# operator on line 5 really necessary in order to return a Boolean from our method?
+
+def valid_series?(nums)
+  return false if nums.sum != 47
+
+  odd_count = nums.count { |n| n.odd? }
+  odd_count == 3
+end
+
+p valid_series?([5, 6, 2, 7, 3, 12, 4, 8])        # should return true
+p valid_series?([1, 12, 2, 5, 16, 6])             # should return false
+p valid_series?([28, 3, 4, 7, 9, 14])             # should return false
+p valid_series?([20, 6, 9, 4, 2, 1, 2, 3])        # should return true
+p valid_series?([10, 6, 19, 2, 6, 4])             # should return false
