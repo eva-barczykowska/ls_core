0f7a53f42e8c581f43c692873bcd6ff9759a74f4
eva-barczykowska <eva.barczykowska@gmail.com>
2022-02-10 15:26:05 +0100

    analyzed code from the questions

---
+++ RB101/lesson_3/practice_problems/medium_6.rb
@@ -0,0 +1,15 @@
+# how this could be simplified?
+
+# def color_valid(color)
+#   if color == "blue" || color == "green"
+#     true
+#   else
+#     false
+#   end
+# end
+
+def color_valid(color)
+  color == "blue" || color == "green"
+end
+
+# Ruby will automatically evaluate statements so this one line is enough
