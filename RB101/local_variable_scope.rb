88b7774d3e09bc9a8ec970f9beaed58d4c677e7e
eva-barczykowska <eva.barczykowska@gmail.com>
2022-02-16 11:46:20 +0100

    notes

---
+++ RB101/local_variable_scope.rb
@@ -0,0 +1,128 @@
+# 1. What is the value of a when  we output it on line 10?
+
+# a = 7
+#
+# def my_value(b)
+#   b += 10
+# end
+#
+# my_value(a)
+# puts a
+
+# *******************************************************************************
+
+# 2. What is the value of a? when we output it on line 22?
+# a = 7
+#
+# def my_value(a)
+#   a += 10
+# end
+#
+# my_value(a)
+# puts a
+
+# *******************************************************************************
+
+# 3. What will the following code print, and why?
+
+a = 7
+
+def my_value(b)
+  a = b
+end
+
+my_value(a + 5)
+puts a
+
+# *******************************************************************************
+
+# 4. What will the following code print, and why?
+
+# a = "Xyzzy"
+#
+# def my_value(b)
+#   b[2] = '-'
+# end
+#
+# my_value(a)
+# puts a
+
+# *******************************************************************************
+
+# 5. What will the following code print, and why?
+
+# a = "Xyzzy"
+#
+# def my_value(b)
+#   b = 'yzzyX'
+# end
+#
+# my_value(a)
+# puts a
+
+# *******************************************************************************
+
+# 6. What will the following code print, and why?
+
+# a = 7
+#
+# def my_value(b)
+#   b = a + a
+# end
+#
+# my_value(a)
+# puts a
+
+# *******************************************************************************
+
+# 7. What will the following code print, and why?
+
+# a = 7
+# array = [1, 2, 3]
+#
+# array.each do |element|
+#   a = element
+# end
+#
+# puts a
+
+## *******************************************************************************
+
+# 8. What will the following code print, and why?
+
+# array = [1, 2, 3]
+#
+# array.each do |element|
+#   a = element
+# end
+#
+# puts a
+
+# *******************************************************************************
+
+# 9. What will the following code print, and why?
+
+# a = 7
+# array = [1, 2, 3]
+#
+# array.each do |a|
+#   a += 1
+# end
+#
+# puts a
+
+# *******************************************************************************
+
+# 10. What will the following code print, and why?
+
+# a = 7
+# array = [1, 2, 3]
+#
+# def my_value(ary)
+#   ary.each do |b|
+#     a += b
+#   end
+# end
+#
+# my_value(array)
+# puts a
