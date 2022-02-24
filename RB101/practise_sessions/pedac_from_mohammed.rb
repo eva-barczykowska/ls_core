6f88a2fe8d5ab86ab82f3023814bf7ff5d7593a3
eva-barczykowska <eva.barczykowska@gmail.com>
2022-02-24 16:31:40 +0100

    pedac from mohammed

---
+++ RB101/practise_sessions/pedac_from_mohammed.rb
@@ -0,0 +1,51 @@
+=begin
+
+
+
+------ PED:- 0 MIN to 12 MIN --------------
+
+
+**Input** :
+**Output** :
+
+**Explicit Requirements/Rules/Boundaries**
+-
+
+
+**Implicit Requirements/Rules/Boundaries**
+-
+
+
+**Special Terms**
+-
+
+
+**Examples/Test Cases**
+- input:
+- Output:
+
+
+**Questions**
+-
+
+
+**Data Structures**
+- input:
+- Output:
+- Intermediate:
+- Storage:
+
+--------------- A:- 12 MIN to 20 MIN------------
+
+**Algorithm**
+-
+
+
+**Implementation details**\
+-
+
+
+-------------- C: 20 MIN to 25 MIN ------------------------
+
+**CodeWithIntent****
+=end
