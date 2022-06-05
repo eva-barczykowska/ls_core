00c4f812329272650af4698c93a7aa6bfc464fb0
eva-barczykowska <eva.barczykowska@gmail.com>
2022-06-05 23:40:12 +0200

    solved

--- RB101/practise_sessions/convert_a_number_to_a_string.rb
+++ RB101/practise_sessions/convert_a_number_to_a_string.rb
@@ -27,47 +27,32 @@
 # output: string
 #
 # Algorithm
-# -divmod to get single digits from the number
+# -define a variable and point it to an empty str, this will be my result
+# - initialize a counter
+# - change the integer into an array of numbers
+# -add in turn those numbers to the result string
+# -well, the numbers are being added as a utf-encoded elemenst like "\u0004\u0003\u0002\u0001"
+# -I need to decode those to be numbers again and add them to the string
+# -I will create an additional variable, an array
+# -I will decode each of those numbers and push them onto the final result array
+# -I will join that array, so that it becomes a string
+# -done!
 #
 # Code
 
-# def integer_to_string(integer)
-#   str = ''
-#   counter = 0
-#   array = integer.digits
-#   while counter < array.size
-#   array.each do |number|
-#     str << number
-#   end
-#       counter += 1
-# end
-#     str
-# end
-#
-# p integer_to_string(4321) #== '4321'
-# integer_to_string(0) == '0'
-# integer_to_string(5000) == '5000'
-
-
-def encode(number)
-  number.encode(utf-8)
-end
 def integer_to_string(integer)
+  final_result = []
   str = ''
   counter = 0
   array = integer.digits.reverse
   while counter < array.size
     str << array[counter]
-          counter += 1
+    counter += 1
   end
-
-    encode(str)
+   str.each_codepoint {|c| final_result <<  c }
+  final_result.join
 end
 
-# p ''
-# p 1.chr(Encoding::UTF_8).class
-
-
-p integer_to_string(4321) #== '4321'
-# integer_to_string(0) == '0'
-# integer_to_string(5000) == '5000'
+p integer_to_string(4321) == '4321'
+p integer_to_string(0) == '0'
+p integer_to_string(5000) == '5000'
