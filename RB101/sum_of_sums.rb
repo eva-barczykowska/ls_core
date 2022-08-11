e09c91adefd72421ff74b9f9958a527d98f351f2
eva-barczykowska <eva.barczykowska@gmail.com>
2022-08-11 23:11:26 +0200

    added some other solutions

--- RB101/sum_of_sums.rb
+++ RB101/sum_of_sums.rb
@@ -31,13 +31,12 @@ Algorithm
 
 -the above will give me [3 + 8 + 10] = 21
 
--so my first sum is 3 or the 1st element of the array,
--then I should iterate over the array and add all the elements until the current counter
-so I always need to add array[0] + counter
+-I should iterate over the array and add all the elements until the current counter
+
 Code
 =end
+
 def sum_of_sums(array)
-  # return array[0].to_i if array.size == 1
   counter = 1
   res = []
   loop do
@@ -46,6 +45,68 @@ def sum_of_sums(array)
     break if counter > array.size
   end
   res.flatten.sum
+end
+
+p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+p sum_of_sums([4]) == 4
+p sum_of_sums([1, 2, 3, 4, 5]) == 35
+
+puts ""
+
+# ls solution 1
+def sum_of_sums(numbers)
+  sum_total = 0
+  accumulator = 0
+
+  numbers.each do |num|
+    accumulator += num
+    sum_total += accumulator # accumulator is the running total that we need to keep track of
+  end
+
+  sum_total
+end
+
+p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+p sum_of_sums([4]) == 4
+p sum_of_sums([1, 2, 3, 4, 5]) == 35
+
+# ls solution 2
+def sum_of_sums(numbers)
+  sum_total = 0
+  1.upto(numbers.size) do |count|
+    sum_total += numbers.slice(0, count).reduce(:+)
+  end
+  sum_total
+end
+
+p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+p sum_of_sums([4]) == 4
+p sum_of_sums([1, 2, 3, 4, 5]) == 35
+
+puts ""
+
+def sum_of_sums(nums)
+  (0...nums.size).reduce(0) do |sum, index|
+    sum + nums[0..index].reduce(:+)
+  end
+end
+
+p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
+p sum_of_sums([1, 5, 7, 3]) == (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) # -> (36)
+p sum_of_sums([4]) == 4
+p sum_of_sums([1, 2, 3, 4, 5]) == 35
+
+puts ""
+
+def sum_of_sums(array)
+  total = 0
+  for i in 0..(array.size - 1)
+    total += array[0..i].sum
+  end
+  total
 end
 
 p sum_of_sums([3, 5, 2]) == (3) + (3 + 5) + (3 + 5 + 2) # -> (21)
