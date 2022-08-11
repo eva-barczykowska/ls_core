7096e0785f2e43395ad53985770ae4bf58552db0
eva-barczykowska <eva.barczykowska@gmail.com>
2022-08-12 00:49:55 +0200

    revised solution

--- RB101/leap_years_2.rb
+++ RB101/leap_years_2.rb
@@ -34,3 +34,30 @@ p leap_year?(1700) == true
 p leap_year?(1) == false
 p leap_year?(100) == true
 p leap_year?(400) == true
+
+puts ""
+
+#other solutions
+
+def leap_year?(year)
+  return true if pre_gregorian_test(year)
+  (year % 400 == 0 ) || (year % 4 == 0 && year % 100 != 0)
+end
+
+def pre_gregorian_test(year)
+  year < 1753 && year % 4 == 0
+end
+
+p leap_year?(2016) == true
+p leap_year?(2015) == false
+p leap_year?(2100) == false
+p leap_year?(2400) == true
+p leap_year?(240000) == true
+p leap_year?(240001) == false
+p leap_year?(2000) == true
+p leap_year?(1900) == false
+p leap_year?(1752) == true
+p leap_year?(1700) == true
+p leap_year?(1) == false
+p leap_year?(100) == true
+p leap_year?(400) == true
