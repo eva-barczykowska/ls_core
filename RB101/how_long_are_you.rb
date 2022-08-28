ebe66f77e41bf7f9dab509d7693700cde9d4d8d7
eva-barczykowska <eva.barczykowska@gmail.com>
2022-08-28 18:01:43 +0200

    completed exercise

---
+++ RB101/how_long_are_you.rb
@@ -0,0 +1,110 @@
+=begin
+How long are you?
+Write a method that takes a string as an argument, and returns an Array that contains every word from the string, to which you have appended a space and the word length.
+
+You may assume that words in the string are separated by exactly one space, and that any substring of non-space characters is a word.
+
+PEDAC
+Problem
+-write a method
+-the method takes a string as an argument
+-the method returns an array that contains every word from the argument, space and size of that word
+-special characters are part of the word
+
+Examples
+word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
+
+word_lengths("baseball hot dogs and apple pie") ==
+  ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it?
+ 3"]
+
+word_lengths("Supercalifragilisticexpialidocious") ==
+  ["Supercalifragilisticexpialidocious 34"]
+
+word_lengths("") == []
+
+Data Structures
+input -string
+output -array
+
+Algorithm
+-split the string by the space, this will give me words
+-initialize a result array
+-iterate through the words and TRANSFORM each of them in the following way:
+-instead of the word, there should be word + space the size of the word
+-once finished iterating, return the result
+=end
+
+# def word_lengths(string)
+#   array_of_words = string.split
+#   res = []
+#   array = array_of_words.each do |word|
+#     res + word.to_s + " " + word.size.to_s
+#   end
+#   res
+# end
+
+def word_lengths(string)
+  array_of_words = string.split
+  array = array_of_words.map do |word|
+    word + " " + word.size.to_s
+  end
+  array
+end
+
+p word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
+
+p word_lengths("baseball hot dogs and apple pie") ==
+  ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+p word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+p word_lengths("Supercalifragilisticexpialidocious") ==
+  ["Supercalifragilisticexpialidocious 34"]
+
+p word_lengths("") == []
+
+puts ''
+
+# ls solution
+def word_lengths(string)
+  words = string.split
+
+  words.map do |word|
+    word + ' ' + word.length.to_s
+  end
+end
+
+p word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
+
+p word_lengths("baseball hot dogs and apple pie") ==
+  ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+p word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+p word_lengths("Supercalifragilisticexpialidocious") ==
+  ["Supercalifragilisticexpialidocious 34"]
+
+p word_lengths("") == []
+
+puts ""
+
+def word_lengths(string)
+  string.split.map { |word| "#{word} #{word.length}" }
+end
+
+p word_lengths("cow sheep chicken") == ["cow 3", "sheep 5", "chicken 7"]
+
+p word_lengths("baseball hot dogs and apple pie") ==
+  ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+p word_lengths("It ain't easy, is it?") == ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+p word_lengths("Supercalifragilisticexpialidocious") ==
+  ["Supercalifragilisticexpialidocious 34"]
+
+p word_lengths("") == []
+
+puts ""
