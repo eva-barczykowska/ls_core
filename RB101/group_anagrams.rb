7b23774ec16dc4f4cc785641fb77208eea40d731
eva-barczykowska <eva.barczykowska@gmail.com>
2022-09-04 12:14:31 +0200

    analyzed the solution

--- RB101/group_anagrams.rb
+++ RB101/group_anagrams.rb
@@ -41,7 +41,9 @@ middle - hash
 output - arrays of anagrams
 
 Algorithm
+-I got stuck on this and finally saw the solution
 -how I am going to do this
+---pay attention-- this never said it needs to be a METHOD!!!
 -initialize a result hash, key is a word, value is an array(of future anagrams)
 -push on the hash the first words from the input array, so the hash will be hash = {"demo" => []}
 -check the second word - all its letters
@@ -49,7 +51,6 @@ Algorithm
 -see if this word is an anagram with any of the keys in the hash
 -all letters from the word in question have to be present in the word we compare it with(hash key)
 -if it is an anagram with any of the keys in the hash, add it to the values array for that key
- I could create a method to verify if a word is an anagram
 -othewise, push to the hash this word as another key, with value as an empty array
 -after I've exhausted the words in the input array, it's time to return
 -return from the hash only the values - these are the anagrams
@@ -60,12 +61,7 @@ Code
 words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
           'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
           'flow', 'neon']
-
-# def anagrams?(first, second)
-#   second.split("").all?{ |letter| first.include?(letter) }
-# end
-
-
+          
 def anagrams(array)
   h = {}
   h[array.shift] = []
@@ -82,3 +78,43 @@ def anagrams(array)
 end
 
 anagrams(words)
+
+puts ""
+# ls solution
+=begin
+Algorithm
+-iterate over the array
+-the key of the hash is going to be a word alphabetically sorted
+-thye value of that key is going to be an array of anagrams associated with this word, like {"demo"=>["dome", "mode"]}
+-so sort the word in alphabetical order
+-if key already exists, append the current word into the value array
+-otherwise create a new key with the sorted word
+=end
+
+result = {}
+
+words.each do |word|
+  key = word.split('').sort.join
+  if result.has_key?(key)
+    result[key].push(word) #we don't care about the keys, they won't be printed out so we can have them sorted as only reference, values are going to be printed
+  else
+    result[key] = [word]
+  end
+end
+
+result.each_value do |v|
+  puts "------"
+  p v
+end
+=begin
+What we are doing here is amazing:) We are sorting the word alphabetically for our purposes only. That's how we know if words are anagrams,
+Because when they're sorted - they're exacly same, right?
+So that's one thing solved - anagrams.
+Now for the hash.
+If the hash has that sorted version of the word as a key, it means we need to add this word (i.e. its not sorted version) to the values array.
+If the hash does not have that sorted version as a key, we will add it. And it does not matter that it's sorted coz we're not printing it!!!
+In the end, we print only the values from the hash, just as the exercise requires us to do.
+=end
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+          'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+          'flow', 'neon']
