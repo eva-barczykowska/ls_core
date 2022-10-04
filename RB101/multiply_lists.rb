40b8c542c718eef30d3d47136b527bff7e58010b
barczykowska <eva.barczykowska@gmail.com>
2022-10-04 10:08:04 +0200

    further exploration

--- RB101/multiply_lists.rb
+++ RB101/multiply_lists.rb
@@ -47,3 +47,30 @@ def multiply_list(arr1, arr2)
 end
 
 p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+
+puts ""
+
+# ls solution
+def multiply_list(list_1, list_2)
+  products = []
+  list_1.each_with_index do |item, index|
+    p "Item from list_1 is #{item}, index from list_2 is #{index} and item at this index position from list_2 is #{list_2[index]}"
+    products << item * list_2[index]
+  end
+  products
+end
+
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+
+puts ""
+
+# Further Exploration
+# The Array#zip method can be used to produce an extremely compact solution
+# to this method. Read the documentation for zip, and see if you can come up
+# with a one line solution (not counting the def and end lines).
+
+def multiply_list(arr1, arr2)
+  arr1.zip(arr2).map { |array| array[0] * array[1] }
+end
+
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
