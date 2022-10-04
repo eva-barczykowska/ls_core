c0f8c8246fc95bb1a912e1c46c2f72e1c8792762
barczykowska <eva.barczykowska@gmail.com>
2022-10-04 15:13:39 +0200

    added other solutions

--- RB101/multiply_lists.rb
+++ RB101/multiply_lists.rb
@@ -64,6 +64,49 @@ p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
 
 puts ""
 
+# other solutions
+def multiply_list(arr1, arr2)
+  multiplied_arr = []
+
+  index = 0
+  loop do
+    curr_val1 = arr1[index]
+    curr_val2 = arr2[index]
+    multiplied_arr << curr_val1 * curr_val2
+    index += 1
+    break if index == arr1.size
+  end
+
+  multiplied_arr
+end
+
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+
+puts ""
+
+def multiply_list(arr1, arr2)
+  result = []
+  arr1.each_index { |index| result << arr1[index] * arr2[index] }
+  result
+end
+
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+
+puts ""
+
+def multiply_list(arr1, arr2)
+  arr3 = []
+  counter = 0
+  loop do
+    arr3 << arr1[counter] * arr2[counter]
+    counter += 1
+    break if counter == arr1.size
+  end
+  arr3
+end
+
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+
 # Further Exploration
 # The Array#zip method can be used to produce an extremely compact solution
 # to this method. Read the documentation for zip, and see if you can come up
@@ -71,6 +114,14 @@ puts ""
 
 def multiply_list(arr1, arr2)
   arr1.zip(arr2).map { |array| array[0] * array[1] }
+end
+
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+
+puts ""
+
+def multiply_list(arr1, arr2)
+  arr1.zip(arr2).map { |pairs| pairs.inject(:*) }
 end
 
 p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
