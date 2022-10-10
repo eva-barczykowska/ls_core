989db97676e2d6677e041cf1b3485b284e65c0bd
barczykowska <eva.barczykowska@gmail.com>
2022-10-10 21:09:13 +0200

    finished the exercise

---
+++ RB101/reversed_array_part2.rb
@@ -0,0 +1,174 @@
+=begin
+Reversed Arrays (Part 2)
+Write a method that takes an Array, and returns a new Array with the elements
+of the original list in reverse order.
+Do not modify the original list.
+
+You may not use Array#reverse or Array#reverse!, nor may you use the method you wrote in the previous exercise.
+
+Examples:
+reverse([1,2,3,4]) == [4,3,2,1]          # => true
+reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+reverse(['abc']) == ['abc']              # => true
+reverse([]) == []                        # => true
+
+list = [1, 3, 2]                      # => [1, 3, 2]
+new_list = reverse(list)              # => [2, 3, 1]
+list.object_id != new_list.object_id  # => true
+list == [1, 3, 2]                     # => true
+new_list == [2, 3, 1]                 # => true
+
+PEDAC
+Problem
+-write a method that reverses elements of the array
+-the method returns a new array and does not modify the original array
+
+Examples
+Examples:
+reverse([1,2,3,4]) == [4,3,2,1]          # => true
+reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+reverse(['abc']) == ['abc']              # => true
+reverse([]) == []                        # => true
+
+list = [1, 3, 2]                      # => [1, 3, 2]
+new_list = reverse(list)              # => [2, 3, 1]
+list.object_id != new_list.object_id  # => true
+list == [1, 3, 2]                     # => true
+new_list == [2, 3, 1]                 # => true
+
+Data Structures
+input: array
+output: new array but elements are in reverse order
+
+Algorithm
+-initialize an empty array
+-iterate through the input array
+-how to take an element (last element of the array for example) in a non-mutating way (Array#last)
+-append this element to the result array
+-do this until you iterate through the whole array
+-return the result
+
+Code
+=end
+
+def reverse(arr)
+  result = []
+  counter = -1
+  while counter >= -(arr.size)
+    result << arr[counter]
+    counter -= 1
+  end
+  result
+end
+p reverse([1,2,3,4]) == [4,3,2,1]          # => true
+p reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+p reverse(['abc']) == ['abc']              # => true
+p reverse([]) == []                        # => true
+
+p list = [1, 3, 2]                      # => [1, 3, 2]
+p new_list = reverse(list)              # => [2, 3, 1]
+p list.object_id != new_list.object_id  # => true
+p list == [1, 3, 2]                     # => true
+p new_list == [2, 3, 1]                 # => true
+
+puts ""
+
+# ls solution
+def reverse(array)
+  result_array = []
+  array.reverse_each { |element| result_array << element }
+  result_array
+end
+
+p reverse([1,2,3,4]) == [4,3,2,1]          # => true
+p reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+p reverse(['abc']) == ['abc']              # => true
+p reverse([]) == []                        # => true
+
+p list = [1, 3, 2]                      # => [1, 3, 2]
+p new_list = reverse(list)              # => [2, 3, 1]
+p list.object_id != new_list.object_id  # => true
+p list == [1, 3, 2]                     # => true
+p new_list == [2, 3, 1]                 # => true
+
+puts ""
+
+def reverse(array)
+  array.each_with_object([]) {|elem, arr| arr.prepend(elem)} # very interesting use each_with_object
+end
+
+p reverse([1,2,3,4]) == [4,3,2,1]          # => true
+p reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+p reverse(['abc']) == ['abc']              # => true
+p reverse([]) == []                        # => true
+
+p list = [1, 3, 2]                      # => [1, 3, 2]
+p new_list = reverse(list)              # => [2, 3, 1]
+p list.object_id != new_list.object_id  # => true
+p list == [1, 3, 2]                     # => true
+p new_list == [2, 3, 1]                 # => true
+
+puts ""
+
+def reverse(array)
+  array.each_with_object([]){ |element, array| array.unshift(element)}
+end
+
+p reverse([1,2,3,4]) == [4,3,2,1]          # => true
+p reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+p reverse(['abc']) == ['abc']              # => true
+p reverse([]) == []                        # => true
+
+p list = [1, 3, 2]                      # => [1, 3, 2]
+p new_list = reverse(list)              # => [2, 3, 1]
+p list.object_id != new_list.object_id  # => true
+p list == [1, 3, 2]                     # => true
+p new_list == [2, 3, 1]                 # => true
+
+puts ""
+
+def reverse(array)
+  new = []
+  array.each do |el|
+    new.prepend(el)
+  end
+  new
+end
+
+p reverse([1,2,3,4]) == [4,3,2,1]          # => true
+p reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+p reverse(['abc']) == ['abc']              # => true
+p reverse([]) == []                        # => true
+
+p list = [1, 3, 2]                      # => [1, 3, 2]
+p new_list = reverse(list)              # => [2, 3, 1]
+p list.object_id != new_list.object_id  # => true
+p list == [1, 3, 2]                     # => true
+p new_list == [2, 3, 1]                 # => true
+
+def reverse(list)
+  count = 0
+  new_list = list.map do |_|
+    count -= 1
+    list[count]
+  end
+
+  new_list
+end
+
+puts ""
+
+def reverse(array)
+  array.inject([], :unshift) # whoa, look at this!!!
+end
+
+p reverse([1,2,3,4]) == [4,3,2,1]          # => true
+p reverse(%w(a b e d c)) == %w(c d e b a)  # => true
+p reverse(['abc']) == ['abc']              # => true
+p reverse([]) == []                        # => true
+
+p list = [1, 3, 2]                      # => [1, 3, 2]
+p new_list = reverse(list)              # => [2, 3, 1]
+p list.object_id != new_list.object_id  # => true
+p list == [1, 3, 2]                     # => true
+p new_list == [2, 3, 1]                 # => true
