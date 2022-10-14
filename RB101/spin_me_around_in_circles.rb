09bdda96c3ba636dbc25361536ca28fddae0294b
barczykowska <eva.barczykowska@gmail.com>
2022-10-14 10:08:00 +0200

    hello world and str are different objects!

--- RB101/spin_me_around_in_circles.rb
+++ RB101/spin_me_around_in_circles.rb
@@ -7,21 +7,23 @@ as the one passed in as an argument or a different object?
 =end
 
 str = "hello world"
+# p "hello world".equal?(str)
+
 p "before we pass this str as an argument:"
 p str.object_id # object_id 60
 
 def spin_me(str)
   p "inside the method:"
-  p str.object_id # object_id 80
+  p str.object_id # object_id 60
   str.split.each { |word| word.reverse! }.join(" ")
 end
 
 puts ""
-p spin_me("hello world").object_id
-p str.object_id
+p spin_me(str).object_id # object_id 80
+# p str.object_id
 
-# we get different objects, already after calling the #split method within the method, we have a different object
 puts ""
+# we get different objects, already after calling the #split method within the method, we have a different object
 
 # we can check it like this
 def spin_me(str)
@@ -33,6 +35,10 @@ end
 str = 'hello world'
 puts str.object_id # 47435609148580
 puts spin_me(str).object_id # 47435609148360
+
+puts ""
+
+puts "passing array as an argument will mutate the object in place"
 
 puts ""
 
@@ -50,4 +56,4 @@ end
 
 arr = ['hello', 'world']
 puts arr.object_id # 47264354160220
-puts spin_me(arr).object_id # 47264354160220
+puts spin_me(arr).object_id # 47264354160220 -- same object_id!
