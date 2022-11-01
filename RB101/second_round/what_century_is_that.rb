d0fd6b380f45867166f2b91ba4b457d949a36c3d
barczykowska <eva.barczykowska@gmail.com>
2022-11-02 00:01:01 +0100

    analyzed other solutions

--- RB101/second_round/what_century_is_that.rb
+++ RB101/second_round/what_century_is_that.rb
@@ -98,3 +98,112 @@ p century(1127) == '12th'
 p century(11201) == '113th'
 
 puts ""
+
+def century(year)
+  century = (year / 100.0).ceil.to_s
+
+  if century[-2] == '1'
+    century + 'th'
+  elsif century[-1] == '1'
+    century + 'st'
+  elsif century[-1] == '2'
+    century + 'nd'
+  elsif century[-1] == '3'
+    century + 'rd'
+  else
+    century + 'th'
+  end
+end
+
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
+
+puts ""
+
+TEENS =  ('11'..'19').to_a
+
+def century(year)
+  century = ''
+  if year <= 100
+    return '1st'
+  elsif year % 100 == 0
+    century = (year/100).to_s + 'th'
+  elsif TEENS.include?((year/100 + 1).to_s[-2..-1])
+    century = (year / 100 + 1).to_s + 'th'
+  else
+    century = (year / 100 + 1).to_s
+    if century[-1] == '1'
+      century += 'st'
+    elsif century[-1] == '2'
+      century += 'nd'
+    elsif century[-1] == '3'
+      century += 'rd'
+    else
+      century += 'th'
+    end
+  end
+  century
+end
+
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
+
+puts ""
+
+NUM_ENDINGS = { 0 => 'th', 1 => 'st', 2 => 'nd', 3 => 'rd', 4 => 'th',
+                5 => 'th', 6 => 'th', 7 => 'th', 8 => 'th', 9 => 'th' }
+
+def century(year)
+  cen = (year.to_f / 100).ceil
+  if (10..20).include?(cen.to_s.slice(-2, 2).to_i)
+    suffix = 'th'
+  else
+    suffix = NUM_ENDINGS[cen.to_s.slice(-1).to_i]
+  end
+  cen.to_s + suffix
+end
+
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
+
+puts ""
+
+def century(number)
+  century_num = number % 100 == 0 ? (number / 100).to_s : (number / 100 + 1).to_s
+  return century_num += 'th' if century_num[-2] == '1'
+  return century_num += 'st' if century_num[-1] == '1'
+  return century_num += 'nd' if century_num[-1] == '2'
+  return century_num += 'rd' if century_num[-1] == '3'
+  return century_num += 'th'
+end
+
+p century(2000) == '20th'
+p century(2001) == '21st'
+p century(1965) == '20th'
+p century(256) == '3rd'
+p century(5) == '1st'
+p century(10103) == '102nd'
+p century(1052) == '11th'
+p century(1127) == '12th'
+p century(11201) == '113th'
