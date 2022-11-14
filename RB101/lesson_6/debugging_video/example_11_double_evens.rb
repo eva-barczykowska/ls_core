e69f6b3e7d33070d8d4b0044cd41aa9efee84ee1
barczykowska <eva.barczykowska@gmail.com>
2022-11-14 12:57:02 +0100

    learning pry and pry-byebug

---
+++ RB101/lesson_6/debugging_video/example_11_double_evens.rb
@@ -0,0 +1,19 @@
+require 'pry'
+require 'pry-byebug'
+
+# write a method that takes an input an array of integers and returns a new array
+# with the even integers doubled
+
+def double_num(num)
+  binding.pry
+  num + 2
+end
+
+def double_evens(arr)
+  arr.map do |num|
+   binding.pry
+    new_num = num.even? ? double_num(num) : num
+  end
+end
+
+p double_evens([6, 8, 3, 4, 5]) == [1, 4, 3, 8, 5]
---
+++ RB101/lesson_6/debugging_video/example_9_fizz_buzz.rb
@@ -0,0 +1,47 @@
+require 'pry'
+require 'pry-byebug'
+
+# write a method that takes an array of integers as an argument
+# and returns a new array with the integers transformed.
+# If the integer is divisible by 3, it should be replaced by 'Fizz',
+# if the integer is divisible by 5, it should be replaced by 'Buzz',
+# if the integer is divisible by both 3 and 5, it should be replaced by 'FizzBuzz'.
+# If it is divisible by neither, it should remain as it is.
+
+# def fizzbuzz(arr)
+#   arr.map do |num|
+#     binding.pry
+#     if num % 5 == 0 && num % 3 == 0
+#       'FizzBuzz'
+#     elsif num % 5 == 0
+#       'Buzz'
+#     elsif num == 3
+#       'Fizz'
+#     else
+#       num
+#     end
+#   end
+# end
+#
+# p fizzbuzz([1, 3, 5, 6, 7, 8, 10, 3, 15, 9]) #== [1, 'Fizz', 'Buzz', 'Fizz', 7, 8, 'Buzz', 'Fizz', 'FizzBuzz', 'Fizz']
+
+puts
+
+# corrected:
+
+def fizzbuzz(arr)
+  arr.map do |num|
+    # binding.pry
+    if num % 5 == 0 && num % 3 == 0
+      'FizzBuzz'
+    elsif num % 5 == 0
+      'Buzz'
+    elsif num % 3 == 0 # here
+      'Fizz'
+    else
+      num
+    end
+  end
+end
+
+p fizzbuzz([1, 3, 5, 6, 7, 8, 10, 3, 15, 9]) == [1, 'Fizz', 'Buzz', 'Fizz', 7, 8, 'Buzz', 'Fizz', 'FizzBuzz', 'Fizz']
