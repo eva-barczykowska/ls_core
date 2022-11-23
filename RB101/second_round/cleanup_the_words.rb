b8c42c3f56b8c54a846cd09528914fc3174695ef
barczykowska <eva.barczykowska@gmail.com>
2022-11-23 13:23:04 +0100

    added AJ\s solution for the removal or extra spaces from the string

--- RB101/second_round/cleanup_the_words.rb
+++ RB101/second_round/cleanup_the_words.rb
@@ -173,3 +173,29 @@ def cleanup(str)
 end
 
 p cleanup("---what's my +*& line?") #== ' what s my line '
+
+puts
+puts "AJ's fully solution"
+def cleanup(string)
+  p string.object_id # original object ID
+  counter = 0
+  loop do
+    break if counter.eql?(string.size)
+
+    if ALPHABET.include?(string[counter]) == false
+      string.gsub!(string[counter], ' ')
+    end
+
+    counter += 1
+  end
+  # below returns same string or can use the squeeze method; but that
+  # creates/returns a new string if you do not use the destructive
+  # squeeze! version. Noticed most students used just squeeze with no !
+  while string.match('  ')
+    string.gsub!('  ', ' ')
+  end
+  p string.object_id # same object ID
+  string
+end
+
+p cleanup("---what's my +*& line?") == ' what s my line '
