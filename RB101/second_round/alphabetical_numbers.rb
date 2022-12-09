71c10f98285b6c89ea0137aa693e44286514756c
barczykowska <eva.barczykowska@gmail.com>
2022-12-09 17:13:13 +0100

    revised

--- RB101/second_round/alphabetical_numbers.rb
+++ RB101/second_round/alphabetical_numbers.rb
@@ -55,7 +55,7 @@ TO_ALPHA_CONVERSION = TO_NUMBER_CONVERSION.invert # this is how we wanted the ha
 
 p TO_ALPHA_CONVERSION
 
-{0=>"zero", 1=>"one", 2=>"two", 3=>"three", 4=>"four", 5=>"five", 6=>"six", 7=>"seven", 8=>"eight", 9=>"nine", 10=>"ten", 11=>"eleven", 12=>"twelve", 13=>"thirteen", 14=>"fourteen", 15=>"fifteen", 16=>"sixteen", 17=>"seventeen", 18=>"eighteen", 19=>"nineteen"}
+{ 0=>"zero", 1=>"one", 2=>"two", 3=>"three", 4=>"four", 5=>"five", 6=>"six", 7=>"seven", 8=>"eight", 9=>"nine", 10=>"ten", 11=>"eleven", 12=>"twelve", 13=>"thirteen", 14=>"fourteen", 15=>"fifteen", 16=>"sixteen", 17=>"seventeen", 18=>"eighteen", 19=>"nineteen" }
 
 def alphabetic_number_sort(arr)
   conversion = arr.map { |num| TO_ALPHA_CONVERSION[num] }.sort # nums sorted alphabetically
