a03ccd24043bea72ec2c72d5298d459a5b7567f6
barczykowska <eva.barczykowska@gmail.com>
2022-12-10 23:52:35 +0100

    the template that Kim is using

---
+++ RB101/pedac_kim.rb
@@ -0,0 +1,32 @@
+=begin pedac
+
+*************Problem***************************
+Overall goal:
+initial input:
+overall output:
+
+explicit requirements:
+
+
+
+implicit requirements(analize task and examples!):
+
+questions:
+
+*************Examples and Test cases************
+++restate all test cases given
+++add edge cases, 0 and empty:
+
+*************Data Structure********************
+++the steps the data takes in between input
+++and output listed above
+
+************Algorithm*************************
+A. Main Method
+  1.
+  2.
+  3.
+
+
+
+=end
