6e3cc656dd49276c3f6fdc4bf577264e8c88629e
barczykowska <eva.barczykowska@gmail.com>
2023-01-06 23:22:33 +0100

    refactored to be shorter but is it more clear?

--- RB101/second_round/lettercase_counter.rb
+++ RB101/second_round/lettercase_counter.rb
@@ -59,3 +59,24 @@ p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
 p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
 p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
 p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
+
+puts
+
+UPPERCASE = ('A'..'Z').to_a
+LOWERCASE = ('a'..'z').to_a
+
+def letter_case_count(str)
+  result = { lowercase: 0, uppercase: 0, neither: 0 }
+  array = str.split("")
+  array.each do |character|
+    result[:uppercase] += 1 if UPPERCASE.include?(character)
+    result[:lowercase] += 1 if LOWERCASE.include?(character)
+    result[:neither] += 1 if !UPPERCASE.include?(character) && !LOWERCASE.include?(character)
+  end
+  result
+end
+
+p letter_case_count('abCdef 123') == { lowercase: 5, uppercase: 1, neither: 4 }
+p letter_case_count('AbCd +Ef') == { lowercase: 3, uppercase: 3, neither: 2 }
+p letter_case_count('123') == { lowercase: 0, uppercase: 0, neither: 3 }
+p letter_case_count('') == { lowercase: 0, uppercase: 0, neither: 0 }
