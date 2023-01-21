209c940f698efa3723a82a16dfcfbd8131f2cbf0
barczykowska <eva.barczykowska@gmail.com>
2023-01-22 00:51:56 +0100

    revised a few problems

--- RB101/lesson_5/practise_problems_2nd_round.rb
+++ RB101/lesson_5/practise_problems_2nd_round.rb
@@ -190,11 +190,11 @@ hsh.each do |_, value|
   end
 end
 
-puts "Practice Problem 9"
 # Practice Problem 9
 # Given this data structure, return a new array of the same structure
 # but with the sub arrays being ordered
 # (alphabetically or numerically as appropriate) in descending order.
+puts "Practice Problem 9"
 
 arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']]
 res = arr.map do |subarray|
@@ -207,11 +207,11 @@ p res
 
 puts
 
-puts "Practice Problem 10"
 # Practice Problem 10
 # Given the following data structure and without modifying the original array,
 # use the map method to return a new array identical in structure to the original
 # but where the value of each integer is incremented by 1.
+puts "Practice Problem 10"
 
 arr = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
 # expected: [{a: 2}, {b: 3, c: 4}, {d: 5, e: 6, f: 7}]
@@ -226,20 +226,32 @@ end
 p res
 
 puts
+puts ".transform_values method in action:"
+
+arr = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
+p arr
+transformed = arr.map do |hash|
+  hash.transform_values { |value| value += 1 }
+  end
+p transformed
+
+puts
 # ls additional solution
-[{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}].each_with_object([]) do |hsh, arr|
+arr = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
+p arr
+
+res = arr.each_with_object([]) do |hsh, arr|
   incremented_hash = {}
   hsh.each do |key, value|
     incremented_hash[key] = value + 1
   end
   arr << incremented_hash
 end
-p arr
+p res
 # => [{:a=>2}, {:b=>3, :c=>4}, {:d=>5, :e=>6, :f=>7}]
 
 puts
 
-puts "Practice Problem 11"
 # Practice Problem 11
 # Given the following data structure use a combination of methods,
 # including either the select or reject method, to return a new array
@@ -248,7 +260,8 @@ puts "Practice Problem 11"
 
 # arr = [[2], [3, 5, 7, 12], [9], [11, 13, 15]]
 # Expected [[], [3, 12], [9], [15]]
-#
+puts "Practice Problem 11"
+
 arr = [[2], [3, 5, 7, 12], [9], [11, 13, 15]]
 res = arr.map do |subarray|
   subarray.select do |number|
@@ -259,11 +272,12 @@ p arr
 p res
 
 puts
-puts "Practice Problem 12"
+
 # Practice Problem 12
 # Given the following data structure, and without using the Array#to_h method,
 # write some code that will return a hash where the key
 # is the first item in each sub array and the value is the second item.
+puts "Practice Problem 12"
 
 arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
 p arr
@@ -275,6 +289,15 @@ end
 
 p h
 
+puts
+# or
+arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
+p arr
+h = arr.each_with_object({}) { |subarray, h| h[subarray[0]] = subarray[1] }
+p h
+
+puts
+
 # ls solution
 hsh = {}
 arr.each do |item|
@@ -282,117 +305,34 @@ arr.each do |item|
 end
 hsh # => {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
 puts
+
+# Practice Problem 13
+# Given the following data structure, return a new array
+# containing the same sub-arrays as the original
+# but ordered logically by only taking into consideration
+# the odd numbers they contain.
+# expected:  => [[1, 8, 3], [1, 5, 9], [6, 1, 7], [1, 6, 9]]
+
 puts "Practice Problem 13"
-# Practice Problem 13
-# expected return value: {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
-# **** finished here for today, continue tomorrow ---analyze LS solutions as well ****
-# Examples:
-#   before:
-#   [{a: 2}, {b: 3, c: 4}, {d: 5, e: 6, f: 7}]
-# after:
-#   [{a: 3}, {b: 4, c: 5}, {d: 6, e: 7, f: 8}]
-#
-# -----------------------
-# Data Structure: Transforming input array elements into a new array of hashes
-# Algorithm:
-#   - Initialize 'new_arr' to iterating through each array element in the original array to trasform and return results into a new array
-# - Iterate through each hash key/value pair to increment each hash key value by 1
-#
-#
-# -----------------------
-# Code:
-arr = [{a: 2}, {b: 3, c: 4}, {d: 5, e: 6, f: 7}]
-
-my_hash = {d: 5, e: 6, f: 7}
-p my_hash.transform_values { |value| value += 1 }
-
-
-# p new_arr
-# => [{:a=>2}, {:b=>3, :c=>4}, {:d=>5, :e=>6, :f=>7}]
-
-# var = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}].map do |subhash|
-#   subhash.each do |key, integer|
-#     subhash[key] = integer + 1
-#   end
-#   end
-
-
-hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
-
-VOWELS = 'aeouiAEOUI'
-hsh.each { |_, value| value.join.split("").select { |char| p char if VOWELS.include?(char) }}
-
-
-# VOWELS = 'aeoui'
-# hsh.each { |key, value| value.join.split("").each { |char| p char if VOWELS.include?(char) }}
-
-
-hsh.each { |key, value| value.join.split("").each {|char| char =~ /aeiou/ } }
-
-puts
-
-# Practice Problem 11
-# Given the following data structure use a combination of methods,
-# including either the select or reject method,
-# to return a new array identical in structure to the original but
-# containing only the integers that are multiples of 3.
-
-arr = [[2], [3, 5, 7, 12], [9], [11, 13, 15]]
-
-# Expected result: [[], [3, 12], [9], [15]]
-
-threes = arr.map do |subarr|
-  subarr.select do |ele|
-    ele % 3 == 0
-  end
-end
-
-p threes
-p threes == [[], [3, 12], [9], [15]]
-
-# Practice Problem 12
-# Given the following data structure, and without using the Array#to_h method,
-# write some code that will return a hash where the key is the first item
-# in each sub array and the value is the second item.
-
-arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
-# expected return value: {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
-
-h = arr.each_with_object({}) { |subarray, h| h[subarray[0]] = subarray[1] }
-
-p arr
-p h
-p h == {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
-# Practice Problem 13
-# Given the following data structure, return a new array containing the same sub-arrays as the original but ordered logically by only taking into consideration the odd numbers they contain.
-
-# -we want a NEW array
-# -with only odd numbers like 1, 3, 5, 7, etc.
 
 arr = [[1, 6, 9], [6, 1, 7], [1, 8, 3], [1, 5, 9]]
+p arr
 
-# expected: [[1, 8, 3], [1, 5, 9], [6, 1, 7], [1, 6, 9]]
-
-new_arr = arr.sort_by do |subarray|
+res = arr.sort_by do |subarray| # we can sort and on top of that sort only SOME elements of the array, how cool is that
   subarray.select do |number|
     number.odd?
   end
 end
 
-p new_arr
+p res
 
-# Practice Problem 13
-# Given the following data structure, return a new array containing the same sub-arrays as the original but ordered logically by only taking into consideration the odd numbers they contain.
-
-# -we want a NEW array
-# -with only odd numbers like 1, 3, 5, 7, etc.
+# Digging deeper
 
 arr = [[1, 6, 9], [6, 1, 7], [1, 8, 3], [1, 5, 9]]
-
 # expected: [[1, 8, 3], [1, 5, 9], [6, 1, 7], [1, 6, 9]]
 updated_subarray = []
-new_arr = arr.sort_by do |subarray| # [1, 6, 9]
-  updated_subarray = subarray.select do |number| # 1, 6, 9, retn [1,9] which is selection criteria for `sort_by`[1, 7], [1,3] [1,5,9]
+new_arr = arr.sort_by do |subarray| # [1, 6, 9] is analyzed first
+  updated_subarray = subarray.select do |number| # 1, 6, 9, return [1,9] which is selection criteria for `sort_by`[1, 7], [1,3] [1,5,9]
     p number
     p number.odd? # true, false, true
   end
@@ -401,41 +341,55 @@ end
 
 p new_arr
 
-# Precedence
-# [1,9], [1,7], [1,3], [1,5,9]
-# sorted will be [1,3], [1,5,9], [1,7], [1,9]
-#
-# arr = ["9", "8", "7", "10", "11"]
-# p (arr.sort) do |x, y|
-#     y.to_i <=> x.to_i
-#   end
-#
-#
-# Precedence problem, remove the p to get correct output
-# # Expected output: ["11", "10", "9", "8", "7"]
-# # Actual output: ["10", "11", "7", "8", "9"]
-#
-# # "10" => 49
-# # "11" => 49
-# # "7" => 55
-# # "8" => 56
-# # "9" => 57
-# another solution
-#
-# arr = ["9", "8", "7", "10", "11"]
-# p (arr.sort do |x, y|
-#     y.to_i <=> x.to_i
-#   end)
-# maybe save to another file -- precedence
-#
-# also can fix it using the {}, which has highter precedence
-#
-# arr = ["9", "8", "7", "10", "11"]
-# p arr.sort { |x, y| y.to_i <=> x.to_i}
-#
-# also this example
-#
-# p(array.map) do |num|
-#   num + 1 # <Enumerator: [1, 2, 3]:map>
-#
-#   p(array.map { |num| num + 1 })
+# arr = [[1, 6, 9], [6, 1, 7], [1, 8, 3], [1, 5, 9]] is our array argument
+# [1,9], [1,7], [1,3], [1,5,9] these are the numbers selected by the
+# .select method and passed to the sort_by method to be sorted
+# sort will sort the numbers
+# sorted numbers will be [1,3], [1,5,9], [1,7], [1,9]
+# the other number will stay as they were because
+# we are not taking them into consideration
+
+# Practice Problem 14
+# Given this data structure write some code to return an array containing
+# the colors of the fruits, and the sizes of the vegetables.
+# The sizes should be uppercase and the colors should be capitalized.
+puts "Practice Problem 14"
+
+hsh = {
+  'grape' => {type: 'fruit', colors: ['red', 'green'], size: 'small'},
+  'carrot' => {type: 'vegetable', colors: ['orange'], size: 'medium'},
+  'apple' => {type: 'fruit', colors: ['red', 'green'], size: 'medium'},
+  'apricot' => {type: 'fruit', colors: ['orange'], size: 'medium'},
+  'marrow' => {type: 'vegetable', colors: ['green'], size: 'large'},
+}
+
+# expected: [["Red", "Green"], "MEDIUM", ["Red", "Green"], ["Orange"], "LARGE"]
+puts hsh.values
+puts
+
+res = []
+hsh.each do |_, value|
+  if value[:type] == "fruit"
+    res << value[:colors].map { |color| color.capitalize }
+  else value[:type] == "vegetable"
+    res << value[:size].upcase
+  end
+end
+
+p res
+
+puts
+
+# Practice Problem 15
+# Given this data structure write some code to return an array
+# which contains only the hashes where all the integers are even.
+puts "Practice Problem 14"
+arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
+result = arr.map do |hash|
+  # hash.each |key, value|
+  p hash
+# end
+
+end
+
+p result
