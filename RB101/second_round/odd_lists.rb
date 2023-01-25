913ef4af8158cd1420d4987e75fc22c3658ff955
barczykowska <eva.barczykowska@gmail.com>
2023-01-25 12:56:13 +0100

    revised each_with_index and divmod

--- RB101/second_round/odd_lists.rb
+++ RB101/second_round/odd_lists.rb
@@ -64,3 +64,59 @@ p oddities(['abc', 'def']) == ['abc']
 p oddities([123]) == [123]
 p oddities([]) == []
 p oddities([1, 2, 3, 4, 1]) == [1, 3, 1]
+
+puts
+
+# another solution during practise with AJ
+# Odd Lists
+# Write a method that returns an Array that contains every other element of an Array that is passed in as an argument. The values in the returned list should be those values that are in the 1st, 3rd, 5th, and so on elements of the argument Array.
+
+# PEDAC TEMPLATE
+
+# ************** Problem **************
+# -write a method that takes an array as an argument
+# -the method returns an array with every other element of the original array
+# -(starting from the first)
+# -if array argument size is 1, return that array
+# -if it's an empty array, return that array
+
+# Questions:
+
+# ************** Examples **************
+# oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+# oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+# oddities(['abc', 'def']) == ['abc']
+# oddities([123]) == [123]
+# oddities([]) == []
+# oddities([1, 2, 3, 4, 1]) == [1, 3, 1]
+
+# ************** Data Structure **************
+# input: array
+# output: array
+
+# ************** Algorithm **************
+# # -if array argument size is 1, return that array
+# # -if it's an empty array, return that array
+# ---do I need to take care of these 2 above separately?
+# -initialize the `result` array
+# -iterate over the argument array and its index
+# -if the index is odd, add the element at that index to the result array
+# -return the `result` array implicitly
+
+
+# ************** Code **************
+
+def oddities(array)
+  result = []
+  array.each_with_index do |element, index|
+    result << element if index.even?
+  end
+  result
+end
+
+p oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+p oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+p oddities(['abc', 'def']) == ['abc']
+p oddities([123]) == [123]
+p oddities([]) == []
+p oddities([1, 2, 3, 4, 1]) == [1, 3, 1]
--- RB101/second_round/sum_of_digits.rb
+++ RB101/second_round/sum_of_digits.rb
@@ -38,18 +38,37 @@ puts
 
 def sum(int)
   res = []
-  target_size = int.to_s.chars.size
+  target_size = int.to_s.chars.size  # this has to be defined *
   while res.size < target_size
     arr = int.divmod(10)
     res << arr.pop
     int = arr.join.to_i  # we need to change the value of int in every iteration!
-    end
+  end
   res.inject(:+)
 end
 p sum(23) == 5
 p sum(496) == 19
 p sum(123_456_789) == 45
 
+# *
+# this has to be defined *
+# otherwise it will have an impact on the loop (look at line 7)
+# integer value is changing!
+
 # 1st iteration arr = [49, 6], adding 6 to res ==> res = [0, 6].
 # 2nd iteration arr = [4, 9], adding 9 to res ==> res = [0, 6, 9].
 # 3rd iteration arr = [0, 4], adding 4 to res ==> res = [6, 9, 4].
+
+puts
+
+def sum(int)
+  target = []
+  int.to_s.chars.each do |ch|
+    target << ch.to_i
+  end
+  # target.inject(:+) -or-
+  target.reduce(0) { |sum, number| sum + number }
+end
+p sum(23) #== 5
+p sum(496) == 19
+p sum(123_456_789) == 45
