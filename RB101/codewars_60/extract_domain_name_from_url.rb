9b9db0fe9b1fb8e3d052007841e957a3d26be948
barczykowska <eva.barczykowska@gmail.com>
2023-02-13 20:04:58 +0100

    deleted solutions that did not work and left only the one with gsub

--- RB101/codewars_60/extract_domain_name_from_url.rb
+++ RB101/codewars_60/extract_domain_name_from_url.rb
@@ -2,7 +2,8 @@
 # (https://www.codewars.com/kata/514a024011ea4fb54200004b/train/ruby)
 # 5 kyu
 =begin
-Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+Write a function that when given a URL as a string,
+parses out just the domain name and returns it as a string. For example:
 
 domain_name("http://github.com/carbonfive/raygun") == "github"
 domain_name("http://www.zombie-bites.com") == "zombie-bites"
@@ -53,52 +54,17 @@ domain_name("https://www.cnet.com") == "cnet"
 
 # ************** Code **************
 
-def domain_name(str)
-  array = str.split(//)
-  if array.first(5).join == "http:"
-    array = array.drop(7)
-    cutoff_point = array.index(".")
-    return array[0...cutoff_point].join
-  elsif array.first(5).join == "https"
-    array = array.drop(8)
-    cutoff_point = array.index(".")
-    return array[0...cutoff_point].join
-  else
-    array.first(3).join == "www"
-    array = array.drop(4)
-    cutoff_point = array.index(".")
-    return array[0...cutoff_point].join
-  end
+def domain_name(url)
+  url.gsub!('http://', '') # deletes prefix
+  url.gsub!('https://', '') # deletes prefix
+  url.gsub!('www.', '') # deletes
+  url.split(".")[0]
 end
 p domain_name("http://google.com") == "google"
 p domain_name("http://google.co.jp") == "google"
 p domain_name("www.xakep.ru") == "xakep"
 p domain_name("https://youtube.com") == "youtube"
-
-puts
-
-def domain_name(str)
-  array = str.split(//)
-
-  if array.first(5).join == "http:"
-    array = array.drop(7)
-    cutoff_point = array.index(".")
-    result = array[0...cutoff_point]
-  elsif array.first(5).join == "https"
-    array = array.drop(8)
-    cutoff_point = array.index(".")
-    result = array[0...cutoff_point]
-  else
-    # array.first(3).join == "www" # this was not even necessary as pointed out by Amy
-    array = array.drop(4)
-    cutoff_point = array.index(".")
-    result = array[0...cutoff_point]
-  end
-
-  result.join
-end
-p domain_name("http://google.com") == "google"
-p domain_name("http://google.co.jp") == "google"
-p domain_name("www.xakep.ru") == "xakep"
-p domain_name("https://youtube.com") == "youtube"
+p domain_name("http://www.zombie-bites.com") == "zombie-bites"
+p domain_name("https://www.cnet.com") == "cnet"
+p domain_name("icann.org") == "icann"
 
