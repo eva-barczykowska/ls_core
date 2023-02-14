4f786726df0869f6518c8890da4532e6a00f4eee
barczykowska <eva.barczykowska@gmail.com>
2023-02-14 23:14:08 +0100

    solved again today

--- RB101/second_round/right_triangles.rb
+++ RB101/second_round/right_triangles.rb
@@ -62,3 +62,64 @@ def triangle(int)
 end
 
 triangle(5)
+
+puts
+=begin
+Right Triangles
+Write a method that takes a positive integer, n, as an argument, and displays a right triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below) should have one end at the lower-left of the triangle, and the other end at the upper-right.
+PEDAC TEMPLATE
+
+************** Problem **************
+-write a method that takes a number as an argument
+-the method displays a triangle
+-the diagonal side of the triangle has one end at the lower left of the
+ triangle
+-and the other one at the upper right
+-the argument is the number of stars which is the base of the triangle
+
+Questions:
+
+************** Examples **************
+
+triangle(5)
+
+    *
+   **
+  ***
+ ****
+*****
+
+************** Data Structures **************
+input: integer
+output: string
+************** Algorithm **************
+-I will draw this triangle in rows
+-the first row will have only 1 star preceded by spaces
+-then row by row, there will be less spaces and more stars
+-this will continue until the number of the stars reaches the
+ argument number
+
+************** Code **************
+=end
+
+def display_triangle(num)
+  space = " "
+  star = "*"
+  number_of_spaces = num - 1
+  number_of_stars = 1
+
+  loop do
+    print space * number_of_spaces
+    print star * number_of_stars
+    puts
+    number_of_spaces -= 1
+    number_of_stars += 1
+    break if number_of_stars > num
+  end
+end
+
+display_triangle(5)
+display_triangle(9)
+
+
+
