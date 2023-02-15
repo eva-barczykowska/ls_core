8266a9b47819a757e98ce495ed05a03e340c6756
barczykowska <eva.barczykowska@gmail.com>
2023-02-15 12:52:12 +0100

    rock_paper_scissors_bonus_features.rb

--- RB101/lesson_2/rock_paper_scissors_bonus_features.rb
+++ RB101/lesson_2/rock_paper_scissors_bonus_features.rb
@@ -1,14 +1,9 @@
-SCISSORS CUTS PAPER COVERS ROCK
-CRUSHES LIZARD POISONS SPOCK
-SMASHES SCISSORS DECAPITATES LIZZARD
-EATS PAPER DISPROVES SPOCK
-VAPORIZES ROCK CRASHES SCISSORS
-
-defeats = {:rock => ['scissors, lizard'], :paper => ['rock', 'spock'], :scissors => ['paper', 'lizard'],
+#
+VALID_CHOICES = %w[rock paper scissors lizard spock]
+h = {:rock => ['scissors', 'lizard'], :paper => ['rock', 'spock'], :scissors => ['paper', 'lizard'],
            :lizard => ['paper', 'spock'], :spock => ['rock']}
 
-#1. Add Lizzard and Spock to my code
-
+# 1. Add Lizzard and Spock to my code
 # 2. Typing the full word "rock" or "lizard" is tiring.
 # Update the program so the user can type "r" for "rock," "p" for "paper," etc.
 # Note that if you do bonus #1, you'll have two words that start with "s." How do you resolve that?
@@ -19,38 +14,40 @@ defeats = {:rock => ['scissors, lizard'], :paper => ['rock', 'spock'], :scissors
 # Don't add your incrementing logic to display_results.
 # Keep your methods simple; they should perform one logical task — no more, no less.
 #
-# what defeats what - store this info in a hash
-# VALID_CHOICES = %w[rock paper scissors]
-VALID_CHOICES = %w[rock paper scissors lizard spock]
-DEFEATS = {:rock => ['scissors, lizard'], :paper => ['rock', 'spock'], :scissors => ['paper', 'lizard'],
-                    :lizard => ['paper', 'spock'], :spock => ['rock']}
+# what defeats what - store this info in a has
+
 def prompt(message)
   Kernel.puts("=>#{message}")
 end
 
-def win?(first, second)
-  first == "rock" && second == "scissors" ||
-    first == "paper" && second == "rock" ||
-    first == "scissors" && second == "paper"
+# Algorithm
+# extract the key from the hash that equals to the my(player) choice converted to symbol
+# extract the values belonging to this key
+# ask if any of the above values are same like computer choice
+# if any of the values is same, player(I) win
+# else computer wins
+
+def win?(h, my_choice, computer_choice)
+  h.keys.any?(my_choice.to_sym) && h[my_choice.to_sym].any? { |elem| elem == computer_choice }
 end
 
-def display_result(player, computer)
-  if win?(player, computer)
-    prompt("You won!")
-  elsif win?(computer, player)
-    prompt("Computer won!")
-  else
+def display_result(h, player_choice, computer_choice)
+  if player_choice == computer_choice
     prompt("It's a tie!")
+  elsif win?(h, player_choice, computer_choice)
+    prompt("You won!")
+  else
+    prompt("Computer won!")
   end
 end
 
 loop do
-  choice = ""
+  my_choice = ""
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}.")
-    choice = Kernel.gets().chomp()
+    prompt("Choose one: #{ VALID_CHOICES.join(', ') }.")
+    my_choice = Kernel.gets().chomp()
 
-    if VALID_CHOICES.include?(choice)
+    if VALID_CHOICES.include?(my_choice)
       break
     else
       prompt("This is not a valid choice!")
@@ -59,8 +56,8 @@ loop do
 
   computer_choice = VALID_CHOICES.sample
 
-  Kernel.puts "You chose #{choice}, computer chose #{computer_choice}."
-  display_result(choice, computer_choice)
+  Kernel.puts "You chose #{my_choice}, computer chose #{computer_choice}."
+  display_result(h, my_choice, computer_choice)
 
   prompt("Do you want to play again? (y/n)")
   answer = Kernel.gets().chomp()
