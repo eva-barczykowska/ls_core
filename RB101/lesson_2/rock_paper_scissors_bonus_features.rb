bb9acd9c564ae3a3af058e43a90f680aee324f63
barczykowska <eva.barczykowska@gmail.com>
2023-02-19 01:02:16 +0100

    display full word of a choice made by player and computer

--- RB101/lesson_2/rock_paper_scissors_bonus_features.rb
+++ RB101/lesson_2/rock_paper_scissors_bonus_features.rb
@@ -4,9 +4,6 @@
 # EATS PAPER DISPROVES SPOCK
 # VAPORIZES ROCK CRASHES SCISSORS
 
-defeats = {:rock => ['scissors, lizard'], :paper => ['rock', 'spock'], :scissors => ['paper', 'lizard'],
-           :lizard => ['paper', 'spock'], :spock => ['rock']}
-
 #1. Add Lizzard and Spock to my code
 
 # 2. Typing the full word "rock" or "lizard" is tiring.
@@ -21,49 +18,70 @@ defeats = {:rock => ['scissors, lizard'], :paper => ['rock', 'spock'], :scissors
 #
 # what defeats what - store this info in a hash
 # VALID_CHOICES = %w[rock paper scissors]
-VALID_CHOICES = %w[rock paper scissors lizard spock]
-h = {:rock => ['scissors, lizard'], :paper => ['rock', 'spock'], :scissors => ['paper', 'lizard'],
-                    :lizard => ['paper', 'spock'], :spock => ['rock']}
+# VALID_CHOICES = { :rock => "r", :paper => "p", :scissors => "s", :lizard => "l", :spock => "sp" }
+VALID_CHOICES = {["r", "rock"]=>:rock, ["p", "paper"]=>:paper, ["s", "scissors"]=>:scissors, ["l", "lizard"]=>:lizard, ["sp", "spock"]=>:spock}
+
+h = { rock: ['scissors, lizard'], paper: ['rock', 'spock'], scissors: ['paper', 'lizard'],
+                    lizard: ['paper', 'spock'], spock: ['rock'] }
+
 def prompt(message)
   Kernel.puts("=>#{message}")
 end
 
 # Algorithm
-# extract the key from the hash that equals to the my(player) choice converted to symbol
-# extract the values belonging to this key
-# ask if any of the above values are same like computer choice
-# if any of the values is same, player(I) win
-# else computer wins
+# check in the hash what player chose and what computer choose, if they're same, it's a tie
 
-def win?(h, my_choice, computer_choice)
-  h.keys.any?(my_choice.to_sym) && h[my_choice.to_sym].any? { |elem| elem == computer_choice }
+def win?(h, player_choice, computer_choice)
+  player_choice == h.keys.any? && computer_choice == h.values.any? # h contains winning combinations for the player
 end
 
 def display_result(h, player_choice, computer_choice)
-  if player_choice == computer_choice
-    prompt("It's a tie!")
-  elsif win?(h, player_choice, computer_choice)
+  if win?(player_choice, computer_choice)
     prompt("You won!")
-  else
+  elsif win?(h, player_choice, computer_choice)
     prompt("Computer won!")
+  else
+    prompt("It's a tie!")
   end
 end
-
+# VALID_CHOICES = {["r", "rock"]=>:rock, ["p", "paper"]=>:paper, ["s", "scissors"]=>:scissors, ["l", "lizard"]=>:lizard, ["sp", "spock"]=>:spock}
 loop do
-  my_choice = ""
+  player_choice = ""
   loop do
-    prompt("Choose one: #{ VALID_CHOICES.join(', ') }.")
-    my_choice = Kernel.gets().chomp()
+    prompt("Choose one, r for rock, p for paper, s for scissors, l for lizard or sp for spock")
+    player_choice = Kernel.gets().chomp()
 
-    if VALID_CHOICES.include?(my_choice)
+    def get_full_word(shortcut)
+      case shortcut
+      when "r"
+        "rock"
+      when "p"
+         "paper"
+      when "s"
+        "scissors"
+      when "l"
+        "lizard"
+      when "sp"
+        "spock"
+      end
+    end
+
+    player_choice = get_full_word(player_choice) # I want to display full form in the terminal for both players
+
+    if VALID_CHOICES.keys.any? { |subarray| subarray.include?(player_choice) }
       break
     else
       prompt("This is not a valid choice!")
     end
+
   end
-    computer_choice = VALID_CHOICES.sample
-    Kernel.puts "You chose #{my_choice}, computer chose #{computer_choice}."
-    display_result(h, my_choice, computer_choice)
+
+    computer_choice = VALID_CHOICES.values.flatten.sample
+
+    Kernel.puts "You chose #{player_choice}, computer chose #{computer_choice}." #display full forms here
+    display_result(h, player_choice, computer_choice)
+
+  # You chose r, computer chose sp.
 
   prompt("Do you want to play again? (y/n)")
   answer = Kernel.gets().chomp()
