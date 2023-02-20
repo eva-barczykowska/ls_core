eb1f6996901423a3e15a14cd918f4c791d520496
barczykowska <eva.barczykowska@gmail.com>
2023-02-20 20:17:41 +0100

    fixed score counting, completed all

--- RB101/lesson_2/rock_paper_scissors_bonus_features.rb
+++ RB101/lesson_2/rock_paper_scissors_bonus_features.rb
@@ -1,6 +1,6 @@
 # SCISSORS CUTS PAPER COVERS ROCK
 # CRUSHES LIZARD POISONS SPOCK
-# SMASHES SCISSORS DECAPITATES LIZZARD
+# SMASHES SCISSORS DECAPITATES LIZARD
 # EATS PAPER DISPROVES SPOCK
 # VAPORIZES ROCK CRASHES SCISSORS
 
@@ -14,11 +14,11 @@
 #    Don't add your incrementing logic to display_results.
 #    Keep your methods simple; they should perform one logical task — no more, no less.
 
-VALID_CHOICES = {["r", "rock"] => :rock, ["p", "paper"] => :paper, ["s", "scissors"] => :scissors, ["l", "lizard"] => :lizard, ["sp", "spock"] => :spock}
+VALID_CHOICES = { ["r", "rock"] => :rock, ["p", "paper"] => :paper, ["s", "scissors"] => :scissors, ["l", "lizard"] => :lizard, ["sp", "spock"] => :spock }
 
-# if computer selected any values from the array belonging to the key that player had selected, player wins
+# if computer selected any values from the array belonging to the key that player had selected (from this hash), player wins
 h = { rock: ['scissors', 'lizard'], paper: ['rock', 'spock'], scissors: ['paper', 'lizard'],
-                    lizard: ['paper', 'spock'], spock: ['rock'] }
+                    lizard: ['paper', 'spock'], spock: ['rock', 'scissors'] }
 
 def prompt(message)
   Kernel.puts("=>#{message}")
@@ -26,7 +26,6 @@ end
 
 # at this point both variables player_choice and computer_choice are string objects
 def win?(h, player_choice, computer_choice)
-
   player_choice == "rock" && h[player_choice.to_sym].any? { |word| word == computer_choice } ||
     player_choice == "paper" && h[player_choice.to_sym].any? { |word| word == computer_choice } ||
     player_choice == "scissors" && h[player_choice.to_sym].any? { |word| word == computer_choice } ||
@@ -36,21 +35,24 @@ end
 
 def display_result(h, player_choice, computer_choice)
   if player_choice == computer_choice
-    prompt("It's a tie!")
+    prompt("It's a tie in this round!")
   elsif win?(h, player_choice, computer_choice)
-    prompt("You won!")
+    prompt("YOU won this round!")
   else win?(h, computer_choice, player_choice)
-       prompt("Computer won!")
+       prompt("COMPUTER won this round!")
   end
 end
 
+player_score = 0
+computer_score = 0
+
 loop do
   player_choice = ""
-  loop do
-    prompt("Choose one, r for rock, p for paper, s for scissors, l for lizard or sp for spock")
+  loop do # basic loop - for getting the correct input from the user
+    prompt("Choose one, 'r' for rock, 'p' for paper, 's' for scissors, 'l' for lizard or 'sp' for spock")
     player_choice = Kernel.gets().chomp()
 
-    def get_full_word(shortcut)
+    def get_full_word(shortcut) # could change this to hash but this is easier to read
       case shortcut
       when "r"
         "rock"
@@ -67,22 +69,41 @@ loop do
 
     player_choice = get_full_word(player_choice) # I want to display full form in the terminal for both players
 
-    if VALID_CHOICES.keys.any? { |subarray| subarray.include?(player_choice) }
+    if VALID_CHOICES.keys.any? { |subarray| subarray.include?(player_choice) } # keys is a nested array, we're looking inside all sub-arrays
       break
     else
       prompt("This is not a valid choice!")
     end
+  end # basic loop - ends here
 
-  end
-
-  computer_choice = VALID_CHOICES.values.sample.to_s #have to change sym to str, otherwise we will be comparing str with symbol in the win? method
+  computer_choice = VALID_CHOICES.values.sample.to_s # have to change sym to str, otherwise we will be comparing str with symbol in the win? method
 
   Kernel.puts "You chose #{player_choice}, computer chose #{computer_choice}." #display full forms here
   display_result(h, player_choice, computer_choice)
 
-  prompt("Do you want to play again? (y/n)")
-  answer = Kernel.gets().chomp()
+  if win?(h, player_choice, computer_choice)
+    player_score += 1
+  elsif win?(h, computer_choice, player_choice)
+    computer_score += 1
+  end
+  # puts "--after update"
+  #
+  # Kernel.puts player_score
+  # Kernel.puts computer_score
+
+  if player_score == 3 || computer_score == 3
+    prompt("Score of 3 points has been reached, the grand winner is #{player_score > computer_score ? "YOU" : "COMPUTER"}!")
+    prompt("Do you want to play again? (y/n)")
+    answer = Kernel.gets().chomp()
+    player_score = 0
+    computer_score = 0
+  else
+    prompt("Do you want to play again? (y/n)") # so after every move (and not after we've 3 points have been reached) we are asking the player if they want to continue
+    answer = Kernel.gets().chomp()
+  end
   break unless answer.downcase.start_with?("y")
+
 end
 
 prompt("Thank you for playing. Good bye.")
+
