39f991a73d79c6c4736fa5600f760f67813addd7
barczykowska <eva.barczykowska@gmail.com>
2023-04-02 22:35:15 +0200

    revision, corrected spelling

--- RB101/written_practise/notion_snippets.rb
+++ RB101/written_practise/notion_snippets.rb
@@ -18,21 +18,25 @@ puts test
 
 # On lines 3-7 `passbyvalue` method is defined with parameter, `value`.
 # On line 9, the object that `test` is pointing to is passed to the `puts` method
-# as an argument, this results in `5` being otput and `nil` being returned.
+# as an argument, this results in `5` being output and `nil` being returned.
 # Also, `object_id` method is called on the object that `test` is referencing.
 
-# Subsequently `passbyvalue` method is invoked and the object that `test` is referencing, is passed as an argument. This method call does the following:
+# Subsequently `passbyvalue` method is invoked and the object that `test` is referencing, is passed as an argument.
+# This method call does the following:
 # -it reassigns param to the return value of calling the Integer* method, so now
 # param is pointing to 25
 # -it passes param to the Kernel#print method, this results in printing  and returning`25`
-# -passes the result of invoking object_id on param to the `p` method, this prints and returns object_id of param. So the return value of this method is the `object_id` of param.
+# -passes the result of invoking object_id on param to the `p` method, this prints and returns object_id of param.
+# So the return value of this method is the `object_id` of param.
 
 # Finally, when the object that `test` is referencing is passed to the `puts`
 # method as an argument, integer `5` is printed.
-# The method has not changed the original object because on the first line of the body of the method param was reassigned to point to another integer, `25`.
+# The method has not changed the original object because on the first line of the body of the method param was
+# reassigned to point to another integer, `25`.
 # It would not be anyway possible for this method to mutate its caller because integers are immutable.
-# Printing `object_id`` of param within the method, after reassinging and printing `object_id` method of `test` on line 10 results in different integers being printed and this is because these are different objects since reassignment took place on line 4, within the method.
-
+# Printing `object_id`` of param within the method, after reassigning and printing `object_id` method of `test`
+# on line 10 results in different integers being printed and this is because these are different objects since
+# reassignment took place on line 4, within the method.
 
 
 n = 101.times do |x|
@@ -47,9 +51,12 @@ puts n
 # The block that `#times` takes, denoted by words `do..end` takes a block parameter
 # `n` and within the block body it assigns integer `11` to `n`. It does it 101 times.
 
-# On the last line the object that `n` is referencing is passed to the `puts` method and this results in the integer `101` being output and `nil` being returned.
+# On the last line the object that `n` is referencing is passed to the `puts` method and this results in the
+# integer `101` being output and `nil` being returned.
 
-# ?# The concepts represented here are return value from the method (`times` retunrs its caller) as well as variable shadowing - block scoped variable named the same as outer scope variable makes the outher scoped variable inaccessible and any attempts to access the outer scoped variable will be unsuccessful.
+# ?# The concepts represented here are return value from the method (`times` returns its caller)
+# as well as variable shadowing - block scoped variable named the same as outer scope variable makes the outer scoped
+# variable inaccessible and any attempts to access the outer scoped variable will be unsuccessful.
 
 
 a = 4
@@ -63,15 +70,16 @@ end
 puts a
 puts b
 
-# On line 1 a local variable `a` is initialzed to point to an Integer object `4`.
+# On line 1 a local variable `a` is initialized to point to an Integer object `4`.
 
 # On line 3 the `loop` method is invoked and a block is passed to it, the block is denoted by keywords `do..end`.
-# Within the block `a` is reassigned to point to `5` and `b` is initialized to point to `3`, after which unconditional `break` keyword is used and so the loop
-# is executed only once.
+# Within the block `a` is reassigned to point to `5` and `b` is initialized to point to `3`, after which
+# unconditional `break` keyword is used and so the loop is executed only once.
 
 # On the last 2 lines, the object that `a` is referencing is passed to the `puts`
 # method, which outputs `5` and returns `nil`.
 # On the last line the object that `b` is referencing is passed to the `puts`
-# method, whichresults in `unknown local variable or method error`.
-# This is because outer or main scope where `puts` with the argument `b` is invoked doesn't have access to the inner scope within the `loop` method.
+# method, which results in `unknown local variable or method error`.
+# This is because outer or main scope where `puts` with the argument `b` is invoked doesn't have access to the
+# inner scope within the `loop` method.
 # The concept represented here is variable scope, specifically the fact that a block's inner scope has access to variables initialized in outer scope  but not vice versa.
