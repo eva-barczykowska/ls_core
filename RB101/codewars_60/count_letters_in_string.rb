fd98ac0993715bfd720ba390cff9b9665ea17e0a
barczykowska <eva.barczykowska@gmail.com>
2023-04-02 22:25:48 +0200

    added 2 solutions from the Friday session

--- RB101/codewars_60/count_letters_in_string.rb
+++ RB101/codewars_60/count_letters_in_string.rb
@@ -102,3 +102,27 @@ p letter_count('codewAars') == {:a=>1, :c=>1, :d=>1, :e=>1, :o=>1, :r=>1, :s=>1,
 p letter_count('activity') == {:a=>1, :c=>1, :i=>2, :t=>2, :v=>1, :y=>1}
 p letter_count('arithmetics') == {:a=>1, :c=>1, :e=>1, :h=>1, :i=>2, :m=>1, :r=>1, :s=>1, :t=>2}
 
+puts
+
+def letter_count(word)
+  word.chars.each_with_object(Hash.new(0)) do |char, hash|
+    hash[char.to_sym] += 1
+  end
+end
+
+p letter_count('codewars') == {:a=>1, :c=>1, :d=>1, :e=>1, :o=>1, :r=>1, :s=>1, :w=>1}
+p letter_count('activity') == {:a=>1, :c=>1, :i=>2, :t=>2, :v=>1, :y=>1}
+p letter_count('arithmetics') == {:a=>1, :c=>1, :e=>1, :h=>1, :i=>2, :m=>1, :r=>1, :s=>1, :t=>2}
+
+puts
+
+def letter_count(word)
+  return_hash = word.chars.sort.each_with_object(Hash.new(0)) do |char, hash|
+    hash[char.to_sym] += 1
+  end
+  return_hash.sort.to_h
+end
+
+p letter_count('codewars') == {:a=>1, :c=>1, :d=>1, :e=>1, :o=>1, :r=>1, :s=>1, :w=>1}
+p letter_count('activity') == {:a=>1, :c=>1, :i=>2, :t=>2, :v=>1, :y=>1}
+p letter_count('arithmetics') == {:a=>1, :c=>1, :e=>1, :h=>1, :i=>2, :m=>1, :r=>1, :s=>1, :t=>2}
