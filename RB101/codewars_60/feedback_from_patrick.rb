f6039e903e8bd7e3ee3da84f7cdd07a6ba68b5e3
barczykowska <eva.barczykowska@gmail.com>
2023-04-11 15:04:44 +0200

    feedback in a separate file

---
+++ RB101/codewars_60/feedback_from_patrick.rb
@@ -0,0 +1,22 @@
+=begin
+
+-focus on the process of debugging, line by line
+-be methodical and don't be afraid of it
+-test as you go, test every assumption
+-at the interview, skim all the questions
+-you can comment out ### the portions of the algorithm that are already done
+
+- Think of the interview as a presentation of your process; you want to make
+it transparent what you're thinking and why, and also package it in a way
+that is easy for them to understand and follow. While thinking, talk through
+your solution process. Once you've settled on something, state it clearly,
+and then type it out. Bite-sized ideas are great. Say a sentence or two about
+each line of algorithm or code that you type, before you type. Then type in
+silence to give you and the interviewer a moment of silence to process.
+- When testing as you code, Always communicate what you *expect* the code to do
+*before* running the code. This demonstrates that you are in control of the
+code
+- Test as often as possible for each unique bit of code; err on the side of
+testing too much
+
+ =end
