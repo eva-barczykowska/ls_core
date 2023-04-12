46ceca60fbe6162314f6fccfd643b71dcc4e1ea4
barczykowska <eva.barczykowska@gmail.com>
2023-04-12 13:06:11 +0200

    solved

---
+++ RB101/codewars_60/triple_double.rb
@@ -0,0 +1,6 @@
+Test.assert_equals(triple_double(451999277, 41177722899), 1)
+Test.assert_equals(triple_double(1222345, 12345), 0)
+Test.assert_equals(triple_double(12345, 12345), 0)
+Test.assert_equals(triple_double(666789, 12345667), 1)
+Test.assert_equals(triple_double(10560002, 100), 1)
+Test.assert_equals(triple_double(1112, 122), 0)
