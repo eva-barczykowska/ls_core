e171119a50efe0eff601586e1c9cdec175893e3b
barczykowska <eva.barczykowska@gmail.com>
2023-04-13 22:50:45 +0200

    solved

--- RB101/codewars_60/spiral.rb
+++ RB101/codewars_60/spiral.rb
@@ -1,18 +1,4 @@
 =begin
-Feedback that i often give:
-- Think of the interview as a presentation of your process; you want to make
-it transparent what you're thinking and why, and also package it in a way
-that is easy for them to understand and follow. While thinking, talk through
-your solution process. Once you've settled on something, state it clearly,
-and then type it out. Bite-sized ideas are great. Say a sentence or two about
- each line of algorithm or code that you type, before you type. Then type in
-silence to give you and the interviewer a moment of silence to process.
-- When testing as you code, Always communicate what you *expect* the code to do
-*before* running the code. This demonstrates that you are in control of the
-code
-- Test as often as possible for each unique bit of code; err on the side of
-testing too much
-=end
 
 # An array of integers is a SPIRAL if the integers increase to a single max
 # value, and then decreases down from there. Write a method that returns
