2553fdceae9e581840bb608a122ce1d3e60940ec
barczykowska <eva.barczykowska@gmail.com>
2023-05-19 17:19:32 +0200

    solved

--- RB101/interview_assessment.rb
+++ RB101/interview_assessment.rb
@@ -1,4 +1,4 @@
-# INTERVIEW ASSESSMENT
+ # INTERVIEW ASSESSMENT
 #
 # - Zoom & Coderpad
 # - One hour. Two problems.
@@ -20,10 +20,48 @@
 # Whitespace and punctuation shall simply be removed!
 # The input is restricted to contain no numerals and only words containing
 # the english alphabet letters.
+#
+# PEDAC TEMPLATE
+#
+# ************** Problem **************
+# -write a method that takes a string as an argument
+# -the method returns a string as well, but it's reordered
+# -it's supposed to be case-insensitive order of appearance like in the alphabet
+# non-alphabet characters are removed
+# spaces get removed
+# ************** Examples **************
+# p alphabetized("The Holy Bible") == "BbeehHilloTy"
+# # =>capital B comes before b
+# p alphabetized("!@$%^&*()_+=-`,") == ""
+# # => no alphabetical characters, hence an empty string is returned
+# p alphabetized("CodeWars can't Load Today") == "aaaaCcdddeLnooorstTWy"
+# # =>removed apostrophe and sorted alphabetically
+# ************** Data Structures **************
+# Input: string
+# Output: string, non-alphabetical characters removed, string sorted alphabetically, capital letters first
+# ************** Algorithm **************
+# -remove non-alphabetical characters
+# -if there aren't any characters left, return an empty string
+# -organize character in alphabetical way, capital letters first
+# ************** Code **************
+#
+# ************** Refactor **************
+def alphabetized(str)
+  alphabet = ("a".."z").to_a + ("A".."Z").to_a
+  str.chars.each do |char|
+    if !alphabet.include?(char)
+      str.delete!(char)
+    end
+  end
+  str.chars.sort { |a, b| a.downcase <=> b.downcase }.join
+end
 
-p alphabetized("The Holy Bible") == "BbeehHilloTy"
-p alphabetized("!@$%^&*()_+=-`,") == ""
-p alphabetized("CodeWars can't Load Today") == "aaaaCcdddeLnooorstTWy"
+p alphabetized("The Holy Bible") #== "BbeehHilloTy"
+# =>capital B comes before b
+p alphabetized("!@$%^&*()_+=-`,") #== ""
+# # => no alphabetical characters, hence an empty string is returned
+p alphabetized("CodeWars can't Load Today") #== "aaaaCcdddeLnooorstTWy"
+# =>removed apostrophe and sorted alphabetically
 
 # Have the method letter_changes(str) take the str parameter being passed and
 # modify it using the following algorithm.
@@ -33,7 +71,7 @@ p alphabetized("CodeWars can't Load Today") == "aaaaCcdddeLnooorstTWy"
 # (ie. c becomes f, Z becomes C).
 # Then return this modified string.
 
-p letter_changes("this long cake@&") == "wklv orqj fdnh@&"
-p letter_changes("Road trip9") == "Urdg wuls9"
-p letter_changes("EMAILZ@gmail.com") == "HPDLOC@jpdlo.frp"
-p letter_changes('xyz') == ('abc')
+# p letter_changes("this long cake@&") == "wklv orqj fdnh@&"
+# p letter_changes("Road trip9") == "Urdg wuls9"
+# p letter_changes("EMAILZ@gmail.com") == "HPDLOC@jpdlo.frp"
+# p letter_changes('xyz') == ('abc')
