919e7443ee38d2f1b5f568f86447b715fa8f430f
barczykowska <eva.barczykowska@gmail.com>
2023-05-24 11:40:59 +0200

    added other solutions from syed and saurabh

--- RB101/interview_assessment.rb
+++ RB101/interview_assessment.rb
@@ -168,3 +168,147 @@ p letter_changes("this long cake@&") == "wklv orqj fdnh@&"
 p letter_changes("Road trip9") == "Urdg wuls9"
 p letter_changes("EMAILZ@gmail.com") == "HPDLOC@jpdlo.frp"
 p letter_changes('xyz') == ('abc')
+
+ puts
+
+#from Syed
+ LOWER = ("a".."z").to_a
+ p LOWER[23]
+ UPPER = ("A".."Z").to_a
+
+ def get_letter(letter)
+   current_index = LOWER.index(letter.downcase) # this is just to get index
+   current_index = current_index - 26 if current_index > 22 # this is to shift index to the beginning if index > 22 (x, y z)
+   letter == letter.downcase ? LOWER[current_index + 3] : UPPER[current_index + 3]
+ end
+
+ p get_letter("x") == "a"
+ p get_letter("y") == "b"
+ p get_letter("z") == "c"
+
+ p get_letter("X") == "A"
+ p get_letter("Y") == "B"
+ p get_letter("Z") == "C"
+
+ def letter_changes(str)
+   result = ""
+   str.each_char do |char|
+     result << get_letter(char) if LOWER.include?(char.downcase)
+     result << char unless LOWER.include?(char.downcase)
+   end
+   result
+ end
+
+ p letter_changes("this long cake@&") == "wklv orqj fdnh@&"
+ p letter_changes("Road trip9") == "Urdg wuls9"
+ p letter_changes("EMAILZ@gmail.com") == "HPDLOC@jpdlo.frp"
+ p letter_changes('xyz') == ('abc')
+
+ puts
+# from Saurabh Kamboj
+=begin
+  Breakdown
+    - Every word in the given string has to be iterated over
+    - Every letter in the word is to be replaced
+    - The letter is replaced by the letter 3 spaces ahead of it
+    - Only operate over alphabets and account for case
+    - Input: String
+    - Output: String
+
+  Data structure
+    - 'abc' > 'def'
+    - a = 1, d = 4, 1 + 3
+    - b = 2, 2 + 3 = 5, e = 5
+    - 1, 2, 3 > 4, 5, 6
+    - End of the string needs to repeat
+    - English alphabets - 26
+    - If letter_number + 3 > 26
+     - new_letter_number = letter_number + 3 - 26
+     - Z = 26, 26 + 3 - 26, 3 = c
+
+  Problem
+    - Iterate over each word
+    - Iterate over each letter in the word
+    - Replace each letter based on algorithm
+    - Join the letters and words, return that as result
+
+  Algorithm
+    - Iterate over each word of the given string
+      - Iterate over each letter of letter of the current word
+      - If the letter is an alphabet
+        - Replace the letter
+      - Else return letter
+      - Join the resultant array
+    - Join the resultant array with spaces as separator
+    - To replace the letter
+      - If ALPHABET_NUMBERS[letter] + 3 > 26
+        new_letter_number = ALPHABET_NUMBERS[letter] + 3 - 26
+      - Else
+        new_letter_number = ALPHABET_NUMBERS[letter] + 3
+      - ALPHABET_NUMBER.key(new_letter_number)
+=end
+
+ ALPHABETS = ('a'..'z').zip(1..26).to_h
+ p ALPHABETS
+ def letter_changes(string)
+   string.split.map do |word|
+     word.chars.map do |letter|
+       if ALPHABETS.key?(letter.downcase)
+         new_letter = change_letter(letter.downcase)
+         letter.downcase == letter ? new_letter : new_letter.upcase
+       else
+         letter
+       end
+     end.join
+   end.join(' ')
+ end
+
+ def change_letter(letter)
+   letter_value = ALPHABETS[letter]
+   if letter_value + 3 > 26
+     ALPHABETS.key((letter_value - 26) + 3)
+   else
+     ALPHABETS.key(letter_value + 3)
+   end
+ end
+
+ p letter_changes("this long cake@&") == "wklv orqj fdnh@&"
+ p letter_changes("Road trip9") == "Urdg wuls9"
+ p letter_changes("EMAILZ@gmail.com") == "HPDLOC@jpdlo.frp"
+ p letter_changes('xyz') == ('abc')
+
+ puts
+
+# Alternate 1
+
+ def letter_changes(string)
+   string.chars.map do |letter|
+     if ALPHABETS.key?(letter.downcase)
+       new_letter = change_letter(letter.downcase)
+       letter.downcase == letter ? new_letter : new_letter.upcase
+     else
+       letter
+     end
+   end.join
+ end
+
+ puts
+# Alternate 2 based on Syed's input
+
+ def letter_changes(string)
+   string.chars.map do |letter|
+     ALPHABETS.key?(letter.downcase) ? change_letter(letter) : letter
+   end.join
+ end
+
+ def change_letter(letter)
+   value = ALPHABETS[letter.downcase] + 3
+   value = value - 26 if value > 23
+
+   letter.downcase == letter ? ALPHABETS.key(value) : ALPHABETS.key(value).upcase
+ end
+
+ p letter_changes("this long cake@&") == "wklv orqj fdnh@&"
+ p letter_changes("Road trip9") == "Urdg wuls9"
+ p letter_changes("EMAILZ@gmail.com") == "HPDLOC@jpdlo.frp"
+ p letter_changes('xyz') == ('abc')
