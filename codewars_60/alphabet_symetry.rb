dd2c7988800adca8beee4283c6d653da9ac78173
barczykowska <eva.barczykowska@gmail.com>
2023-08-02 10:04:01 +0200

    removed redundant snippet

--- codewars_60/alphabet_symetry.rb
+++ codewars_60/alphabet_symetry.rb
@@ -251,17 +251,6 @@ p solve(["abode","ABc","xyzD"]) == [4,3,1]
 p solve(["abide","ABc","xyz"]) == [4,3,0]
 p solve(['IAMDEFANDJKL','thedefgh','xyzDEFghijabc']) == [6,5,7]
 p solve(["encode","abc","xyzD","ABmD"]) == [1, 3, 1, 3]
-ALPHA = Array('a'..'z').zip(1..26).to_h
-
-def solve(arr)
-  arr.map do |word|
-    word
-      .downcase
-      .each_char
-      .with_index
-      .count { |ch, i| ALPHA[ch] == i + 1 }
-  end
-end
 
 
 
