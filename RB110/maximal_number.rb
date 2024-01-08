93a4e3269d1cb6a816b8f7fee0985eba66dc1d70
barczykowska <eva.barczykowska@gmail.com>
2024-01-08 10:14:51 +0100

    come back to this

--- RB110/maximal_number.rb
+++ RB110/maximal_number.rb
@@ -1,65 +1,74 @@
 =begin
 Given an integer n, find the maximal number you can obtain by deleting exactly one digit of the given number.
+Example
+For n = 152, the output should be 52;
+For n = 1001, the output should be 101.
+Input/Output
 
+    [input] integer n
+
+    Constraints: 10 ≤ n ≤ 1000000.
+
+    [output] an integer
+
+PEDAC:
 P:
--write a method that takes an integer
--the method returns the maximal number that I can obtain by deleting exactly 1 number from the argument
+- write a method that takes an integer n
+- if we remove any of the digits from this integer, what is the maximal_number that we can get?
+
+-return that number
 
 E:
-delete_digit(791983) == 91983
-91983
-71983
-79983
-79183
-79193
-79198
-=>91983
-expected = [91983, 71983, 79983, 79183, 79193, 79198]
-delete_digit(152) == 52
-delete_digit(1001) == 101
-delete_digit(10) == 1
+maximal_number(152) == 52
+52, 15, 12 => 52
+maximal_number(1001) == 101
+001, 101, 101, 100 => 101
 
 DS:
-input: integer
-output: integer
+input: int
+medium: array
+output:integer
 
 Notes:
+-digit
+
 A:
-- create `numbers` array
+-initialize `possible_numbers` array where I will store target numbers to choose from
 
-# - change the int argument into string and then array
+- Convert input integer into an array of separate integers
 
-# - loop over the array
-# - initialize `counter`
-# - use index to iterate over the array and at each iteration select elements of the array at each index BUT the index
-    that is equal to counter
-# - join that array and append it to `numbers`
-# - increase the counter
+- initialize a while loop and iterate while counter < array.size
+- inside the loop create a duplicate of the `numbers_array`, this will be always a starting point
 
-# - transform the array of string-numbers into integers
-# - check which number in `numbers` is the greatest and return it
+- iterate over the digits of the number and remove each consecutive digit:
+
+- take copy array - remove digit at index 0, -> store copy without the num at index in `possible_numbers` array
+- take copy array - remove digit at index 1, -> store copy without the num at index in `possible_numbers` array
+- take copy array - remove digit at index 2, -> store copy without the num at index in `possible_numbers` array
+etc.
+
+- iterate over `possible_numbers` and select this array, whose sum is the biggest
+- return that number
+152 = > 52 12, 15
 =end
 
-def delete_digit(n)
-  numbers = []
+def maximal_number(number)
+  possible_numbers = []
 
-  array = n.to_s.chars
+  numbers_array = number.digits.reverse
+  copy = numbers_array.dup # making a copy of the object
 
   counter = 0
-  while counter < array.size
-    numbers << array.select.with_index { |_, index| index != counter }.join
+
+  while counter < numbers_array.size
+    copy = numbers_array.dup
+    copy.delete_at(counter)  #working on the copy object, not on the elements that the copy contains
+    possible_numbers << copy
     counter += 1
   end
 
-  numbers.map { |e| e.to_i }.sort.last
-
+  possible_numbers.map { |array| array.join.to_i }.max
 end
-p delete_digit(791983) == 91983
-p delete_digit(152) == 52
-p delete_digit(1001) == 101
-p delete_digit(10) == 1
+p maximal_number(152) == 52
+p maximal_number(1001) == 101
 
-# Feedback
-# SLOW DOWN when bugs are encountered
-# Don't worry about speed, practise coding with intention
-# Stay calm when things don't work as expected and debug
