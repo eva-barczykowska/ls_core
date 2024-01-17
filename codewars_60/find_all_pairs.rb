cdc18dc6b97c7a14c39d0916015c853068652c38
barczykowska <eva.barczykowska@gmail.com>
2024-01-17 09:04:32 +0100

    solved

--- codewars_60/find_all_pairs.rb
+++ codewars_60/find_all_pairs.rb
@@ -160,6 +160,188 @@ p pairs([1000, 1000]) == 1
 p pairs([]) == 0
 p pairs([54]) == 0
 
+puts
+
+# =begin
+# You are given array of integers, your task will be to count all pairs in that array and return their count.
+
+# Notes:
+
+# Array can be empty or contain only one value; in this case return 0
+# If there are more pairs of a certain number, count each pair only once. E.g.: for [0, 0, 0, 0] the return value is 2 (= 2 pairs of 0s)
+# Random tests: maximum array length is 1000, range of values in array is between 0 and 1000
+
+# [1, 2, 5, 6, 5, 2]  -->  2
+# ...because there are 2 pairs: 2 and 5
+
+# [1, 2, 2, 20, 6, 20, 2, 6, 2]  -->  4
+
+# ...because there are 4 pairs: 2, 20, 6 and 2 (again)
+# PEDAC
+# P:
+# - write a method that takes an array of integers as an argument
+# - method counts pairs and returns the count
+# - if array is empty or only 1 integers, return 0
+# - each pair is counted oly once,[0, 0, 0, 0] the return value is 2 (= 2 pairs of 0s)
+
+# E:
+# pairs([1, 2, 5, 6, 5, 2]) == 2
+# => 2,2,5,5
+# pairs([1, 2, 2, 20, 6, 20, 2, 6, 2]) == 4
+# =>2,2,2,2,20,20,6,6
+
+# DS:
+# input: array of integers
+# output: integer: count of how many pairs we have
+
+# A:
+# - initialize result to 0
+# - count the integers, how many of each I have, store results in a hash
+# - iterate over the hash, if the value for a certain key is even/ divide it by 2 and add the result to `result`
+# - return result
+# =end
+
+# def pairs(array)
+#   result = 0
+#   counts = array.tally
+#   counts.each do |int, count|
+#     if count.even?
+#       result += count / 2
+#     end
+#   end
+
+#   result
+# end
+# p pairs([1, 2, 5, 6, 5, 2]) == 2
+# p pairs([1, 2, 2, 20, 6, 20, 2, 6, 2]) == 4
+
+# puts
+
+# def pairs(array)
+#   result = 0
+
+#   array.tally.each { |int, count| result += count / 2 if count.even? }
+
+#   result
+# end
+# p pairs([1, 2, 5, 6, 5, 2]) == 2
+# p pairs([1, 2, 2, 20, 6, 20, 2, 6, 2]) == 4
+
+# # without tally
+# # initializean empty hash
+# # iterate over the array
+# # if hash doesn't have the int at the current iteration as a key,
+# #   add it as a key and assign value 1
+# #   -if it has, add 1 to the value
+
+# def get_counts(array)
+#   counts = {}
+#   array.each do |int|
+#    if !counts.key?(int)
+#      counts[int] = 1
+#    else
+#      counts[int] += 1
+#    end
+#   end
+#   counts
+# end
+
+# # p get_counts([1, 2, 5, 6, 5, 2])
+
+# def pairs(array)
+#   result = 0
+
+#   counts = get_counts(array)
+
+#  counts.each do |int, count|
+#   if count.even?
+#     result += count / 2
+#   end
+# end
+
+# result
+# end
+# p pairs([1, 2, 5, 6, 5, 2]) == 2
+# p pairs([1, 2, 2, 20, 6, 20, 2, 6, 2]) == 4
+
+puts
+#with each_with_object
+
+def pairs(array)
+  counts = array.each_with_object({}) { |int, hash| hash[int] = array.count(int) }
+
+  result = 0
+  counts.each do |int, count|
+    if count.even?
+      result += count / 2
+    end
+  end
+
+  result
+end
+
+p pairs([1, 2, 5, 6, 5, 2]) == 2
+p pairs([1, 2, 2, 20, 6, 20, 2, 6, 2]) == 4
+
+#error - why does it not allow me to use this object that it returns further but I have to assign it to something?
+# And what is this big number? The docs say the method returns the memo object so why can't I use it further?
+
+# def pairs(array)
+#   array.each_with_object({}) { |int, hash| hash[int] = array.count(int) }
+#
+#   result = 0
+#   hash.each do |int, count|
+#     if count.even?
+#       result += count / 2
+#     end
+#   end
+#
+#   result
+# end
+#
+# p pairs([1, 2, 5, 6, 5, 2]) == 2
+# p pairs([1, 2, 2, 20, 6, 20, 2, 6, 2]) == 4
+
+
+puts
+#solve using method chaining?
+
+#error - why does it not allow me to use this object that it returns further but I have to assign it to something?
+# And what is this big number? The docs say the method returns the memo object so why can't I use it further?
+
+# NO, error is undefined method `each' for 4466104106738790135:Integer
+
+# def pairs(array)
+#   result = 0
+#   array.each_with_object({}) { |int, hash| hash[int] = array.count(int) }.each do |int, count|
+#     result += count / 2 if count.even?
+#   end
+#
+#   result
+# end
+#
+# p pairs([1, 2, 5, 6, 5, 2]) == 2
+# p pairs([1, 2, 2, 20, 6, 20, 2, 6, 2]) == 4
+
+def pairs(array)
+  result = 0
+  hash = array.each_with_object({}) { |int, hash| hash[int] = array.count(int) }
+
+  hash.each do |int, count|
+    result += count / 2 if count.even?
+  end
+
+  result
+end
+
+p pairs([1, 2, 5, 6, 5, 2]) == 2
+p pairs([1, 2, 2, 20, 6, 20, 2, 6, 2]) == 4
+
+
+
+
+
+
 
 
 
