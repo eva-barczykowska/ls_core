54f9858cc13534a8e228cfddc8876ce1be7424f8
barczykowska <eva.barczykowska@gmail.com>
2024-01-23 21:58:51 +0100

    3 level nested iteration

--- RB110/combo_me_3.rb
+++ RB110/combo_me_3.rb
@@ -24,8 +24,8 @@ end
 p combo_me(3, ["a", "b", "c", "d", "e"]) == [["a", "b", "c"], ["a", "b", "d"], ["a", "b", "e"], ["a", "c", "d"], ["a", "c", "e"], ["a", "d", "e"], ["b", "c", "d"], ["b", "c", "e"], ["b", "d", "e"], ["c", "d", "e"]]
 
 puts
-# Option long without Combination, pattern repeats itself for additional levels
-array_to_combo =  ["a", "b", "c", "d", "e"]
+# Long option without Combination, pattern repeats itself for additional levels
+array_to_combo = ["a", "b", "c", "d", "e"]
 
 length_array_to_combo = array_to_combo.length
 long_way_results_array = []
@@ -41,3 +41,18 @@ long_way_results_array
 
 
 p combo_me(3, ["a", "b", "c", "d", "e"]) == [["a", "b", "c"], ["a", "b", "d"], ["a", "b", "e"], ["a", "c", "d"], ["a", "c", "e"], ["a", "d", "e"], ["b", "c", "d"], ["b", "c", "e"], ["b", "d", "e"], ["c", "d", "e"]]
+
+# else it can be done like this
+###### Option long without Combination, pattern repeats itself for additional levels
+length_array_to_combo = array_to_combo.length
+long_way_results_array = []
+
+(0...length_array_to_combo).each do |idx_1|
+  (idx_1...length_array_to_combo).each do |idx_2|
+    (idx_2...length_array_to_combo).map do |idx_3|
+      next if idx_3 <= idx_2 || idx_2 <= idx_1
+      long_way_results_array << [array_to_combo[idx_1], array_to_combo[idx_2], array_to_combo[idx_3]]
+    end
+  end
+end
+long_way_results_array
