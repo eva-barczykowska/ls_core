f08b2871afcb1877adccf8801ff483aeb591652c
barczykowska <eva.barczykowska@gmail.com>
2024-03-04 10:10:20 +0100

    added solution from Ilke

--- codewars_60/expanded_form.rb
+++ codewars_60/expanded_form.rb
@@ -70,5 +70,104 @@ puts expanded_form(12) == '10 + 2'
 puts expanded_form(308) == '300 + 8'
 puts expanded_form(5304) == '5000 + 300 + 4'
 
+puts
+# You will be given a number and you will need to return it as a string in expanded form. For example:
+#
+# expanded_form(12); # Should return '10 + 2'
+# expanded_form(42); # Should return '40 + 2'
+# expanded_form(70304); # Should return '70000 + 300 + 4'
+#
+# Note: All numbers will be whole numbers greater than 0.
 
+# Problem:
+# -we get an integer
+# -we need to return that integers in expanded form 
+# -expanded form means ones—tens—hundreds—thousands
 
+# Examples:
+# # expanded_form(12); # Should return '10 + 2'
+# => 2 in place of ones and 1 in place of tens
+
+# # expanded_form(42); # Should return '40 + 2'
+# => 2 in place  of ones and 4 in place of tens
+
+# # expanded_form(70304); # Should return '70000 + 300 + 4'
+
+# 4 in places of ones
+# 0 in place of tens
+# 3 in place of hundreds
+# 0 in place of thousands
+# 7 in place of 10_000
+# => the more to the LEFT we go, the more zeros we multiply the number by
+
+# 4*1=4
+# 0*10=0
+# 3*100=3
+# 0*1000=0
+# 7*10000=70000
+
+# Hypothesis/Algorithm:
+# - I will change the integer to an array of digits
+# - I will iterate over this array:
+# - The first time I iterate, I multiply number * 1 and append this to my result array, result array is [4]
+# - I will increase the multiplier (it was 1) by 10 and now multiply the 2nd number, which is 0 by it
+# - The result is 0 and I prepend this to the result array, now result array is [0, 4]
+# - Then I will increase the multiplier again by ten, so multiplier * 10 and prepend the result of multiplication
+# - I will get [3, 0, 4], then  [0, 3, 0, 4] and finally  [70000, 0, 300, 0, 4]
+# - Now I  get rid of single zeros, if zeros are there, verify first
+# - Join on “+“
+
+def expanded_form(int)
+  digits = int.digits
+  result = []
+
+  multiplier = 1
+  counter = 0
+  
+  loop do 
+    result << digits[counter] * multiplier
+    multiplier *= 10
+    counter += 1
+    break if counter > digits.size - 1
+  end
+  
+  result.none? {|e| e.zero? } ? result.join(" + ") : result.reject! { |e| e.zero? }.join(" + ")
+end
+
+p expanded_form(12) == '10 + 2'
+p expanded_form(42) == '40 + 2'
+p expanded_form(70304) == '70000 + 300 + 4'
+
+puts "Ilke's solution"
+# - a way to break up th integer
+#   - convert to array of chars
+#   - reverse array, index position now corresponds to number of 0s needed after digit
+# - storage array for the different parts
+#     - add to storage array unless char is "0"
+# - join elements in storage array with " + "
+
+# Algo:
+# - initialize a storage array
+# - convert input integer to a string
+# - then convert to an array of chars 
+# - then reverse array
+# - iterate through array with index
+#   - add char + "0" * index to storage array, UNLESS char == "0"
+# - join elements of storage array up with " + "
+# - reverse and return resulting string
+
+def expanded_form(input_int)
+  storage_array = []
+  arr = input_int.to_s.chars.reverse
+
+  arr.each_with_index do |char, index|
+    storage_array << char + "0" * index unless char == "0"
+  end
+
+  storage_array.reverse.join(" + ")
+end
+p expanded_form(12) == '10 + 2'
+p expanded_form(42) == '40 + 2'
+p expanded_form(70304) == '70000 + 300 + 4'
+
+#so this aproach is to convert input integer to a string, then to characters and reverse those. 
