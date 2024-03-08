7dfc9585a2bcd2daa3f70f5dbe243f4458e33617
barczykowska <eva.barczykowska@gmail.com>
2024-03-08 10:54:38 +0100

    added older breakup and algorithm

--- codewars_60/part_list.rb
+++ codewars_60/part_list.rb
@@ -81,6 +81,63 @@ end
 p partlist(["az", "toto", "picaro", "zone", "kiwi"]) == [["az", "toto picaro zone kiwi"], ["az toto","picaro zone kiwi"], ["az toto picaro","zone kiwi"], ["az toto picaro zone", "kiwi"]]
 
 puts
+=begin
+Write a method partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+ 
+Each two non empty parts will be in an an array: see Examples test Cases
+Each part will be in a string
+Elements of a pair must be in the same order as in the original array.
+ 
+P:
+-method takes an array of strings
+-method that gives all the ways to divide an array
+-array has at least 2 elements
+-into 2 non-empty parts
+-each part is a string
+-elements must be in the same order as in the original array
+-return array will have arrays or pairs, which indicate the way to divide
+ 
+E:
+a = ["az", "toto", "picaro", "zone", "kiwi"] ==
+[["az", "toto picaro zone kiwi"] => 1 string separately, all others together as 1 string - 4 words
+["az toto", "picaro zone kiwi"], => 2 string separately, all others together as 1 string - 3 words
+["az toto picaro", "zone kiwi"], => 3 string separately, all others together as 1 string - 2 words
+["az toto picaro zone", "kiwi"]] => 4 string separately, all others together as 1 string - 1 words
+ 
+4
+1, 2, 3, 4
+ 
+N:
+p a = ["az", "toto", "picaro", "zone", "kiwi"][0..2].join(" ")
+ 
+A:
+-initialize `result`
+ 
+-gradually add to result arrays of pairs of strings:
+-first string gets gradually longer, second string gets gradually shorter:
+ -initialize a counter 0
+ -initialize `start` at 0
+  -initialize `finish` at 0
+ 
+ -start a loop
+ 
+ -initialize temp to which you append:
+  --a range from the arg array
+  --ALWAYS start at 0, end at 0 (only fist word), end changes, we will start at 0 and increase immediately after
+  --join - you have the first string of 2, add this first string to `temp`
+  --increase `first_counter` --you have first array, join the array and add the resulting string to `temp`
+  --start at `finish`, end at -1, join and add, you have second string for the `temp` array
+  --start is now
+ 
+  --a range from the arg array
+  --start at 0, end at 1(`finish`) (only fist word)
+  --join - you have the SECOND array of 2
+  --increase `finish` --you have first array, after add it to `result`
+ 
+p ["az", "toto", "picaro", "zone", "kiwi"][0..0]
+p ["az", "toto", "picaro", "zone", "kiwi"][1..-1]
+def partlist(arr)
+=end
 
 def artlist(arr)
    result = []
@@ -123,3 +180,6 @@ def partlist(arr)
   result
   end
 p partlist(["az", "toto", "picaro", "zone", "kiwi"]) == [["az", "toto picaro zone kiwi"], ["az toto","picaro zone kiwi"], ["az toto picaro","zone kiwi"], ["az toto picaro zone", "kiwi"]]
+
+puts
+puts "earlier solution"
