35e86c28d0817843775f9b0e7bacb027fc157e62
barczykowska <eva.barczykowska@gmail.com>
2024-03-11 23:29:56 +0100

    further exploration rotate string added

--- RB110/medium_rotation_part1.rb
+++ RB110/medium_rotation_part1.rb
@@ -1,5 +1,5 @@
 =begin
-Rotation (Part 1) 23:04
+Rotation (Part 1) 23:04-23:14
 Write a method that rotates an array by moving the first element to the end of the array. The original array should not be modified.
 
 Do not use the method Array#rotate or Array#rotate! for your implementation.
@@ -43,3 +43,42 @@ p rotate_array(['a']) == ['a']
 x = [1, 2, 3, 4]
 p rotate_array(x) == [2, 3, 4, 1]
 p x == [1, 2, 3, 4]
+
+puts
+#but they wanted THE SAME array, in that case 
+def rotate_array(array)
+  array[1..-1] + [array[0]]
+end
+
+# Further Exploration
+# Write a method that rotates a string instead of an array. Do the same thing for integers. You may use rotate_array from inside your new method.
+def rotate(str)
+  str = str[1..-1] << str[0] # reassigning
+end
+
+name = 'anaconda'
+p rotate(name) #== 'nacondaa'
+p name #== 'anaconda'
+
+puts
+
+def rotate(str)
+  str = str[1..-1] += str[0] # reassigning
+end
+
+name = 'anaconda'
+p rotate(name) #== 'nacondaa'
+p name #== 'anacondaa'. # extra 'a'! -- not good
+
+puts
+
+name = 'anaconda'
+def rotate(str)
+  copy = str.dup # making a copy
+  first = copy[0]
+  copy.reverse.chop.reverse << first
+end
+
+name = 'anaconda'
+p rotate(name) #== 'nacondaa'
+p name #== 'anaconda'
