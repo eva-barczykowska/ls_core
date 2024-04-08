e4441dea8343fe048ba2c0d046102f64bdbb42f0
barczykowska <eva.barczykowska@gmail.com>
2024-04-08 08:53:31 +0200

    other solutions

--- codewars_60/string_expansion.rb
+++ codewars_60/string_expansion.rb
@@ -1,115 +1,111 @@
-=begin
-Given a string that includes alphanumeric characters ("3a4B2d") return the expansion of that string: The numeric values represent the occurrence of each letter preceding that numeric value. There should be no numeric characters in the final string.
-
-Notes
-The first occurrence of a numeric value should be the number of times each character behind it is repeated, until the next numeric value appears
-If there are multiple consecutive numeric characters, only the last one should be used (ignore the previous ones)
-Empty strings should return an empty string.
-Your code should be able to work for both lower and capital case letters.
-
-P:
--method takes a string with numbers
--method returns only letters, each letter is printed the number of times indicated by the first integer BEFORE it
--code should work for both upper and lowercase
-
-E:
-"3D2a5d2f"  -->  "DDDaadddddff"
-=>                DDDaadddddff
-
-"3abc"      -->  "aaabbbccc"
-=>                aaabbbccc
-
-"3d332f2a"  -->  "dddffaa"
-                  dddffaa
-
-"abcde"     -->  "abcde" 
-=> no numbers, return str argument
-
-"1111"      -->  ""
-=> all numbers, return empty str
-
-""          -->  ""
-=> empty str, return empty str
-
-DS:
-input: str with numbers
-
-output: str without numbers
-
-N: reverse_each 
----------------------------IMPORTANT FEEDBACK FROM WOOK-----------------------------------
-----test verbally
-----come up with pattern that takes care of ALL the testcases
-----come up with a new patterns/new ideas
-----practise coming up with different ideas
----------------------------IMPORTANT FEEDBACK FROM WOOK-----------------------------------
-A:
-- initialize `new_str`
-- iterate over the string
- - if
-
-s = "3D2a5d2f"
-
+# Given a string that includes alphanumeric characters ("3a4B2d") return the expansion of that string: The numeric values represent the occurrence of each letter preceding that numeric value. There should be no numeric characters in the final string.
+#
+# Notes
+# The first occurrence of a numeric value should be the number of times each character behind it is repeated, until the next numeric value appears
+# If there are multiple consecutive numeric characters, only the last one should be used (ignore the previous ones)
+# Empty strings should return an empty string.
+# Your code should be able to work for both lower and capital case letters.
+#
+# P:
+# -method takes a string with numbers
+# -method returns only letters, each letter is printed the number of times indicated by the first integer BEFORE it
+# -code should work for both upper and lowercase
+#
+# E:
+# "3D2a5d2f"  -->  "DDDaadddddff"
+# =>                DDDaadddddff
+#
+# "3abc"      -->  "aaabbbccc"
+# =>                aaabbbccc
+#
+# "3d332f2a"  -->  "dddffaa"
+#                   dddffaa
+#
+# "abcde"     -->  "abcde"
+# => no numbers, return str argument
+#
+# "1111"      -->  ""
+# => all numbers, return empty str
+#
+# ""          -->  ""
+# => empty str, return empty str
+#
+# DS:
+# input: str with numbers
+#
+# output: str without numbers
+#
+# N: reverse_each
+# ---------------------------IMPORTANT FEEDBACK FROM WOOK-----------------------------------
+# ----test verbally
+# ----come up with pattern that takes care of ALL the testcases
+# ----come up with a new patterns/new ideas
+# ----practise coming up with different ideas
+# ---------------------------IMPORTANT FEEDBACK FROM WOOK-----------------------------------
+# A:
+# - initialize `new_str`
+# - iterate over the string
+#  - if
+#
+# s = "3D2a5d2f"
+#
 # p str_expansion("3D2a5d2f") == "DDDaadddddff"
 # p str_expansion("3abc") == "aaabbbccc"
 # p str_expansion("3d332f2a") == "dddffaa"
-=end
 puts
-=begin
-Given a string that includes alphanumeric characters ("3a4B2d") return the expansion of that string: The numeric values represent the occurrence of each letter preceding that numeric value. There should be no numeric characters in the final string.
-
-Notes
-The first occurrence of a numeric value should be the number of times each character behind it is repeated, until the next numeric value appears
-If there are multiple consecutive numeric characters, only the last one should be used (ignore the previous ones)
-Empty strings should return an empty string.
-Your code should be able to work for both lower and capital case letters.
-
-P:
--method takes an array as an argument
--method builds a new string
--the number immediately preceding the letter indicates how many times that letter will be pushed to the new string
--empty string returns ""
--code needs to work for both lowercase and uppercase characters
-
-E:
-p str_expansion("3D2a5d2f") == "DDDaadddddff"
-=>                DDD-aa-ddddd-ff
-p str_expansion("3abc") == "aaabbbccc"
-                 
-p str_expansion("3d332f2a") == "dddffaa"
-
-Idea1
-Iterate over this string and look for the last number before the letter. Use that number to append that many letters as the number indicates to `result` string.
-If the next letter doesn't have a number in front of it, use the number that you've already saved (as `multiplier`) to append that many letters as the multiplier indicates.
-
-Idea2
-Change into array or chars and chunk based on i, j, j.to_i == 0. This will give me a nested array with arrays inside. The arrays will have a number and a letter.
- Use that number as a multiplier for the letter. However, if that array has only one array inside, take the first element, a number and use it as a multiplier for all the remaining elements(letters) in that array. Multiply and append to `result.`
-
- Idea3
- Reverse the string. Iterate over the characcters and find the first number, it and multiply the first letter by the first char you can find after it, remove 
-
-
-DS:
-input: str with numbers
-output: str without numbers but with multiplied characters
-
-A:
-Going with Idea 2
--init `result_str`
--change str to arr and chunk on the basis of integer, you'll get nested array to process
--first element of the inner array is always the multiplier, then there are letters
--use the multiplier to multiply the letters and append them to the string
--if there's no 2nd element, we need to skip somehow
-
-
-
-=end
+# Given a string that includes alphanumeric characters ("3a4B2d") return the expansion of that string: The numeric values represent the occurrence of each letter preceding that numeric value. There should be no numeric characters in the final string.
+#
+# Notes
+# The first occurrence of a numeric value should be the number of times each character behind it is repeated, until the next numeric value appears
+# If there are multiple consecutive numeric characters, only the last one should be used (ignore the previous ones)
+# Empty strings should return an empty string.
+# Your code should be able to work for both lower and capital case letters.
+#
+# P:
+# -method takes an array as an argument
+# -method builds a new string
+# -the number immediately preceding the letter indicates how many times that letter will be pushed to the new string
+# -empty string returns ""
+# -code needs to work for both lowercase and uppercase characters
+#
+# E:
+# p str_expansion("3D2a5d2f") == "DDDaadddddff"
+# =>                DDD-aa-ddddd-ff
+# p str_expansion("3abc") == "aaabbbccc"
+#
+# p str_expansion("3d332f2a") == "dddffaa"
+#
+# Idea1
+# Iterate over this string and look for the last number before the letter. Use that number to append that many letters as the number indicates to `result` string.
+# If the next letter doesn't have a number in front of it, use the number that you've already saved (as `multiplier`) to append that many letters as the multiplier indicates.
+#
+# Idea2
+# Change into array or chars and chunk based on i, j, j.to_i == 0. This will give me a nested array with arrays inside. The arrays will have a number and a letter.
+#  Use that number as a multiplier for the letter. However, if that array has only one array inside, take the first element, a number and use it as a multiplier for all the remaining elements(letters) in that array. Multiply and append to `result.`
+#
+#  Idea3
+#  Reverse the string. Iterate over the characcters and find the first number, it and multiply the first letter by the first char you can find after it, remove
+#
+#
+# DS:
+# input: str with numbers
+# output: str without numbers but with multiplied characters
+#
+# A:
+# Going with Idea 2
+# -init `result_str`
+# -change str to arr and chunk on the basis of integer, you'll get nested array to process
+# -first element of the inner array is always the multiplier, then there are letters
+# -use the multiplier to multiply the letters and append them to the string
+# -if there's no 2nd element, we need to skip somehow
+#
+#
+#
 
 def str_expansion(str)
   result_str = ''
 
-  chunks = str.chars.chunk_while { |i, j| j.to_i == 0 }.to_a
+  chunks = str.chars.chunk_while { |_i, j| j.to_i == 0 }.to_a
   chunks.each do |array_inside|
     multiplier = array_inside[0].to_i
     array_inside[1..-1].each do |letter|
@@ -117,9 +113,122 @@ def str_expansion(str)
     end
   end
 
-result_str
-
+  result_str
 end
-p str_expansion("3D2a5d2f") == "DDDaadddddff"
-p str_expansion("3abc") == "aaabbbccc"
-p str_expansion("3d332f2a") == "dddffaa"
+p str_expansion('3D2a5d2f') == 'DDDaadddddff'
+p str_expansion('3abc') == 'aaabbbccc'
+p str_expansion('3d332f2a') == 'dddffaa'
+
+puts
+
+# A:
+# -define number-strings constant `NUMERICS`
+# -define the `str_expansion` method
+# -INIT `result`
+# -INIT `multiplier`
+#
+# -ITERATE over str argument
+# -for each character:
+# --if it belongs to `NUMERICS`, set multiplier to this character
+# --else multiply character by multiplier and append the result to `result`
+NUMERICS = ('0'..'9')
+def str_expansion(str)
+  result = ''
+  multiplier = 1
+
+  str.each_char do |char|
+    case NUMERICS.include?(char)
+    when true  then multiplier = char.to_i
+    when false then result << char * multiplier
+    end
+  end
+
+  result
+end
+
+p str_expansion('3D2a5d2f') == 'DDDaadddddff'
+p str_expansion('3abc') == 'aaabbbccc'
+p str_expansion('3d332f2a') == 'dddffaa'
+
+puts
+
+# A:
+# -INIT `NUMBER_STRINGS`
+# -define the `str_expansion` method
+# -INIT `result`
+# -INIT `multiplier`
+# -INIT `counter`
+#
+# -ITERATE over str argument
+# -for each character:
+# --if it is a number-string, SET multiplier to this character
+# --else multiply character by multiplier and append the result to `result`
+NUMBER_STRINGS = ('0'..'9')
+def str_expansion(str)
+  result = ''
+  multiplier = 1
+
+  counter = 0
+  loop do
+    if NUMBER_STRINGS.include?(str[counter])
+      multiplier = str[counter].to_i
+    else
+      result << str[counter] * multiplier
+    end
+    counter += 1
+    break if counter > str.size - 1
+  end
+
+  result
+end
+
+p str_expansion('3D2a5d2f') == 'DDDaadddddff'
+p str_expansion('3abc') == 'aaabbbccc'
+p str_expansion('3d332f2a') == 'dddffaa'
+
+puts
+
+NUMBER_STRINGS = ('0'..'9')
+def str_expansion(str)
+  result = ''
+  multiplier = 1
+
+  counter = 0
+  loop do
+    current_character = str[counter]
+    if NUMBER_STRINGS.include?(current_character)
+      multiplier = current_character.to_i
+    else
+      result << current_character * multiplier
+    end
+    counter += 1
+    break if counter > str.size - 1
+  end
+
+  result
+end
+
+p str_expansion('3D2a5d2f') == 'DDDaadddddff'
+p str_expansion('3abc') == 'aaabbbccc'
+p str_expansion('3d332f2a') == 'dddffaa'
+
+puts
+puts 'generated by AI'
+NUMBER_STRINGS = ('0'..'9')
+def str_expansion(str)
+  result = ''
+  multiplier = 1
+
+  str.chars.each do |char|
+    if NUMBER_STRINGS.include?(char)
+      multiplier = char.to_i
+    else
+      result << char * multiplier
+    end
+  end
+
+  result
+end
+p str_expansion('3D2a5d2f') == 'DDDaadddddff'
+p str_expansion('3abc') == 'aaabbbccc'
+p str_expansion('3d332f2a') == 'dddffaa'
