9deb25b5eacfe3b97454393ac88b31070b3e5ad0
barczykowska <eva.barczykowska@gmail.com>
2024-05-03 11:15:06 +0200

    feedback from Amar, solution from codewars

--- codewars_60/triple_trouble.rb
+++ codewars_60/triple_trouble.rb
@@ -179,3 +179,143 @@ p triple_double(10_560_002, 100) == 1
 p triple_double(1112, 122) == 0
 p triple_double(451_999_277, 41_177_722_899) == 1
 p triple_double(1_222_345, 12_345) == 0
+
+puts
+# Algorithm
+# =========
+# GET unique nums [1,2,3,4,5]
+# CHANGE num1 and num2 to strings
+# ITERATE over the unique numbers and query num1 if it has a tripple of a number and num2 if it has a double
+# -multiply num by 1000 for the num1 and multiply num by 100 for num2, convert to str
+# -scan the num1 and num2 for these values,
+# RETURN if match is found
+#
+# -else RETURN 0
+
+def triple_double(num1, num2)
+  unique_nums = num1.digits.reverse.uniq
+  str1 = num1.to_s
+  str2 = num2.to_s
+
+  unique_nums.each do |num|
+    triple = num * 111
+    double = num * 11
+    return 1 if !str1.scan(triple.to_s).empty? && !str2.scan(double.to_s).empty?
+  end
+
+  0
+end
+p triple_double(111_222_345, 122_345) == 1
+p triple_double(133_345, 123_345) == 1
+p triple_double(12_345, 12_345) == 0
+p triple_double(666_789, 12_345_667) == 1
+p triple_double(10_560_002, 100) == 1
+p triple_double(1112, 122) == 0
+p triple_double(451_999_277, 41_177_722_899) == 1
+p triple_double(1_222_345, 12_345) == 0
+
+#
+# feedback from Amar
+#
+# When working through your examples, asking yourself questions like
+#  "I need to check my first string for a triple and second string for a double - how do i do that?"
+# " What are all of my valid triples and valid doubles?"
+# " How can I access these valid triples and valid doubles?"
+# " How can I compare any valid triples to valid doubles?"
+#
+# This type of thinking can really help your algorithm because you're addressing needs that can help you develop an algorithm that will perform all the rules you define. Notice that all of these questions are ambiguous in that there can be multiple different solutions -- it's not important to think of the answers during the Example phase of PEDAC, but it can help you generate ideas and also some of these questions may be easily answerable and help speed up your process.
+# For example, "How can I access these valid triples and valid doubles?" In the back of my head, I'm already thinking of using arrays to store all valid triples and all valid doubles. I don't know what I'm going to do with these arrays, but having them identified and available for use helps me structure my solving process, because I know I can use them in a variety of ways to come up with a solution. Here the most important thing is that I don't have a specific solution, but rather I can implement a variety of solutions depending on what I brainstorm/come up with.
+# As to your second algorithm, you knew that if you had pre-prepared chunks (i.e. our valid triples and doubles) that you could then somehow compare your triples and doubles to see if an argument (num1 and num2) contain those values. And this comparison is handled with #scan , although there are many ways to go about this logic. It's the general step that leads you to your specific implementation. This is the mentality / process I'm referring to, where you have an idea on how you want to approach your solution. You don't know what you will do, but that doesn't matter, because your approach logically makes sense and therefore will solve the problem. You can change the logic itself a million times, but the approach will always work.
+#
+# Note that although I chose to break up the third step where you were performing the bulk of the logic that is checking for triples and doubles, multiplying them by 1000/100, converting them to strings, and finally scanning num1/num2 for these products -- in reality it may be easier to have a broad general approach all in one step as you did, and then flesh out the specific implementation details. It really just depends on the question itself, your own problem solving process, and how much work is being done by this specific step. I just personally find it easier to break things down into general steps that focus on one-two pieces of logic, rather than having a general step that has multiple processes going on. I find it harder to track what's going on, and I find that my algorithm
+#
+# HAVE A BROAD GENERAL APPROACH
+# ASK QUESTIONS LIKE THOSE MENTIONED AT THE BEGINNING
+
+# - GET unique nums
+# 	# How would I implement this? General step that I need to do, so here I can consider
+# 	a few different options
+# - CHANGE num1 and num2 to strings
+# 	# How would I implement this? General step that I need to do, so here I can consider
+# 	a few different options. Easiest option is `to_s`, but there could be others
+
+# - CHECK if num1 has a triple and num2 has a double
+# 	# How would I implement this? General step that I need to do, so here I can consider
+# 	a few different options
+# 	# This step from your algorithm had a lot of logic going on. It may be worthwhile
+# 	to consider splitting this upto into smaller chunks such as:
+# 	- CHECK if num1 has a triple and num2 has a double
+# 		# After entire general approach is formatted, consider implementation details
+# 	- MULTIPLY triple by 1000 and double by 100
+# 		# After entire general approach is formatted, consider implementation details
+# 	- CONVERT the products to strings
+# 		# After entire general approach is formatted, consider implementation details
+# 	- SCAN/CHECK if num1 and num2 contain the products
+# 		# After entire general approach is formatted, consider implementation details
+# - RETURN 1 if match is found, otherwise 0
+#
+# Amar's algorithm for this problem
+#
+# Algorithm:
+# - CREATE an array of all triples
+# - CREATE an array of all doubles
+# - CREATE two arrays for all valid triples/doubles found in num1/num2
+# - FIND all valid triples
+#   - ITERATE over array of triples
+#     - Check if a string verison of the current num is found in string num1
+#     - if so, move to an tripples
+# - Find all valid doubles
+#   - ITERATE over array of doubles
+#     - check if a string version of the current num is found in string num2
+#     - if so, move to the dpib;es array
+# - SELECT all triple double matches
+#   - ITERATE over tripples array
+#     - for each tripple
+#     - ITERATE over doubles array
+#       - for each double
+#       - if the current double is equal to the first 2 digits of the tripple
+#         - return 1
+# - RETURN 0
+
+def triple_double(num1, num2)
+  array_of_all_tripples = num1.digits.uniq.reverse.map { |d| d.to_s * 3 }
+  array_of_all_doubles = num1.digits.uniq.reverse.map { |d| d.to_s * 2 }
+
+  tripples_from_num1 = array_of_all_tripples.select { |tripple| num1.to_s.include?(tripple) }
+  doubles_from_num2 = array_of_all_doubles.select { |double| num2.to_s.include?(double) }
+
+  tripples_from_num1.each do |tripple|
+    doubles_from_num2.each do |double|
+      return 1 if double == tripple[0..1]
+    end
+  end
+
+  0
+end
+p triple_double(111_222_345, 122_345) #== 1
+p triple_double(133_345, 123_345) == 1
+p triple_double(12_345, 12_345) == 0
+p triple_double(666_789, 12_345_667) == 1
+p triple_double(10_560_002, 100) == 1
+p triple_double(1112, 122) == 0
+p triple_double(451_999_277, 41_177_722_899) == 1
+p triple_double(1_222_345, 12_345) == 0
+
+# codewars solution that I liked
+
+def triple_double(num1, num2)
+  num1 = num1.to_s
+  num2 = num2.to_s
+  10.times do |i| # smart! coz there are only 10 numbers (0-9)
+    return 1 if num1.include?(i.to_s * 3) && num2.include?(i.to_s * 2)
+  end
+  0
+end
+p triple_double(111_222_345, 122_345) #== 1
+p triple_double(133_345, 123_345) == 1
+p triple_double(12_345, 12_345) == 0
+p triple_double(666_789, 12_345_667) == 1
+p triple_double(10_560_002, 100) == 1
+p triple_double(1112, 122) == 0
+p triple_double(451_999_277, 41_177_722_899) == 1
+p triple_double(1_222_345, 12_345) == 0
