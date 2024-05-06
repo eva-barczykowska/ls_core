82423ca57b9afaf876883741f1af664a93b9bd53
barczykowska <eva.barczykowska@gmail.com>
2024-05-06 09:33:32 +0200

    working on logic

--- RB110/minimum_sum.rb
+++ RB110/minimum_sum.rb
@@ -279,3 +279,53 @@ p minimum_sum([1, 2, 3, 4]).nil?
 p minimum_sum([1, 2, 3, 4, 5, 6]) == 15
 p minimum_sum([55, 2, 6, 5, 1, 2, 9, 3, 5, 100]) == 16
 p minimum_sum([-1, -5, -3, 0, -1, 2, -4]) == -10
+
+# working on logic
+# Problem 2
+# Create a method that takes an array of integers as an argument. The method should return the minimum sum of 5 consecutive numbers in the array. If the array contains fewer than 5 elements, the method should return nil.
+#
+# p minimum_sum([1, 2, 3, 4]) == nil
+# p minimum_sum([1, 2, 3, 4, 5, -5]) == 9
+# p minimum_sum([1, 2, 3, 4, 5, 6]) == 15
+# p minimum_sum([55, 2, 6, 5, 1, 2, 9, 3, 5, 100]) == 16
+# p minimum_sum([-1, -5, -3, 0, -1, 2, -4]) == -10
+#
+# Problem
+# =======
+# -method takes an array of integers as an argumenent
+# -method returns min sum of 5 consecutive numbers in the array
+# -if the array contains fewer than 5 numbers, method returns nil
+# -only numbers are elements, positive or negative
+#
+# Examples
+# =========
+# p minimum_sum([1, 2, 3, 4]) == nil
+# => fewer than 5 elements
+# p minimum_sum([1, 2, 3, 4, 5, -5]) == 9
+# => 5 consecutive number will be:
+# [1, 2, 3, 4, 5] => 15
+# [2, 3, 4, 5, -5] => 9 MIN SUM
+# p minimum_sum([1, 2, 3, 4, 5, 6]) == 15
+# [1, 2, 3, 4, 5] => 15 MIN SUM
+# [2, 3, 4, 5, 6] => 20
+# p minimum_sum([55, 2, 6, 5, 1, 2, 9, 3, 5, 100]) == 16
+# [55, 2, 6, 5, 1] => 69
+# [2, 6, 5, 1, 2] => 16 MIN SUM
+# [6, 5, 1, 2, 9] => 23
+# [5, 1, 2, 9, 3] => 20
+# [1, 2, 9, 3, 5] => 20
+# p minimum_sum([-1, -5, -3, 0, -1, 2, -4]) == -10
+# [-1, -5, -3, 0, -1] => - 10 MIN SUM
+# [-5, -3, 0, -1, 2] => 7
+# [-3, 0, -1, 2, -4] => 6
+#
+# Algorithm
+# =========
+# INIT `slices` array
+# CREATE arrays of 5 elements from the array argument
+# --start at index 0 and continue to index -5 -starting index
+# --start at starting index and continue to the end of the array - ending index
+# --slice elements at target indices, store slice in `slices` array
+#
+# TRANSFORM slices into their sum
+# RETURN mininum sum
